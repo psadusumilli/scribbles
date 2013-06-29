32e1a25c54091a3e0598211814aa5e8b36ba893a
vijayrc <vijayrc@outlook.com>
2013-06-29 17:16:45 -0400

    added key value ds sample

--- redis/src/main/java/com/vijayrc/scribble/redis/sample/KeyValueSample.java
+++ redis/src/main/java/com/vijayrc/scribble/redis/sample/KeyValueSample.java
@@ -13,6 +13,18 @@ public class KeyValueSample extends BaseSample{
     @Override
     public void run() {
         jedis.set("key_1", "value_1");
-        log.info("key_1 =>" + jedis.get("key_1"));
+        log.info("key_1 => " + jedis.get("key_1"));
+
+        jedis.append("key_1"," value_1.1");
+        log.info("key_1 => " + jedis.get("key_1"));
+
+        jedis.incr("key_2");
+        log.info("key_2 => " + jedis.get("key_2"));
+        jedis.incrBy("key_2",3);
+        log.info("key_2 => " + jedis.get("key_2"));
+        jedis.decrBy("key_2",2);
+        log.info("key_2 => " + jedis.get("key_2"));
+
+        jedis.del("key_1","key_2");
     }
 }
