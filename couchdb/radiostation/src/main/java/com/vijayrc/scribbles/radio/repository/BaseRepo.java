5a3764b5bd2ac640174a6e0892d010f3acb5587d
vijayrc <vijayrc@outlook.com>
2013-07-06 13:50:04 -0400

    added view 'all' to all docs

--- couchdb/radiostation/src/main/java/com/vijayrc/scribbles/radio/repository/BaseRepo.java
+++ couchdb/radiostation/src/main/java/com/vijayrc/scribbles/radio/repository/BaseRepo.java
@@ -3,8 +3,12 @@ package com.vijayrc.scribbles.radio.repository;
 import com.vijayrc.scribbles.radio.documents.BaseDoc;
 import lombok.extern.log4j.Log4j;
 import org.ektorp.CouchDbConnector;
+import org.ektorp.ViewQuery;
 import org.ektorp.support.CouchDbRepositorySupport;
+import org.ektorp.support.GenerateView;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Log4j
 public abstract class BaseRepo<T extends BaseDoc> extends CouchDbRepositorySupport<T> {
@@ -16,6 +20,17 @@ public abstract class BaseRepo<T extends BaseDoc> extends CouchDbRepositorySuppo
         super(type, db);
         this.type = type;
         initStandardDesignDocument();
+    }
+
+    @Override
+    @GenerateView
+    public List<T> getAll() {
+        return super.getAll();
+    }
+
+    public List<T> getAll(int limit) {
+        ViewQuery q = createQuery("all").limit(limit).includeDocs(true);
+        return db.queryView(q, type);
     }
 
 
