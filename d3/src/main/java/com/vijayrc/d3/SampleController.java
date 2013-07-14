35896d2686b78f1312ffb5fcb24d9b2182502957
vijayrc <vijayrc@outlook.com>
2013-07-14 13:56:36 -0400

    added some d3 controller changes

---
+++ d3/src/main/java/com/vijayrc/d3/SampleController.java
@@ -0,0 +1,16 @@
+package com.vijayrc.d3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SampleController {
+
+    @RequestMapping("/{sample}")
+    public ModelAndView show(@PathVariable String sample){
+        return new ModelAndView(sample);
+    }
+
+}
