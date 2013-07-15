7be4f58bba130f7386f3a362ab91f5c4f273f81c
vijayrc <vijayrc@outlook.com>
2013-07-15 10:55:53 -0400

    starting with album as a doc

---
+++ couchdb/radiostation/src/test/java/com/vijayrc/scribbles/radio/repository/AllSongsTest.java
@@ -0,0 +1,34 @@
+package com.vijayrc.scribbles.radio.repository;
+
+import com.vijayrc.scribbles.radio.documents.Artist;
+import com.vijayrc.scribbles.radio.documents.Song;
+import com.vijayrc.scribbles.radio.util.Random;
+import com.vijayrc.scribbles.radio.vo.Album;
+import lombok.extern.log4j.Log4j;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+@Log4j
+public class AllSongsTest {
+    @Autowired
+    private AllSongs allSongs;
+    @Autowired
+    private AllArtists allArtists;
+
+    public void shouldCheckLinkingOfArtistDocument(){
+        Artist artist = new Artist("test_artist",Random.date(1980,1981));
+        allArtists.add(artist);
+
+        Album album = new Album("test_album",artist, Random.date(2003,2005));
+        Song song = new Song("test_song",album,"jazz",1223);
+        allSongs.add(song);
+
+
+    }
+
+
+}
