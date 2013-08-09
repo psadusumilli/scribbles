ee045d14bc0dadc1d34873e8f9ba4f4baffa2810
vijayrc <vijayrc@outlook.com>
2013-08-08 22:55:49 -0400

    rational example added

--- scala/test/com/vijayrc/reason/RationalTest.scala
+++ scala/test/com/vijayrc/reason/RationalTest.scala
@@ -8,7 +8,12 @@ class RationalTest extends FunSuite{
       val r2 = new Rational(3,5)
       val r3 = r1 + r2;
       assert(r3 == new Rational(11,10))
+  }
 
+  test("should throw exception"){
+    intercept[IllegalArgumentException]{
+      new Rational(4,0)
+    }
   }
 
 
