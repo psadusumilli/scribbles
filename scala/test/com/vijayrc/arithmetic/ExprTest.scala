42e035e42cb9c505c26b2bdba31ebcaed4c894ef
vijayrc <vijayrc@outlook.com>
2013-08-25 23:33:53 -0400

    some more pattern examples

--- scala/test/com/vijayrc/arithmetic/ExprTest.scala
+++ scala/test/com/vijayrc/arithmetic/ExprTest.scala
@@ -32,5 +32,58 @@ class ExprTest extends FunSuite{
     assert(simplified == Variable("x"))
   }
 
+  test("should match different pattern types"){
+
+    //variable
+    val expr = "vijay"
+    val answer = expr match{
+      case "shravan" => "its not the son"
+      case "rekha" => "its not the son"
+      case someoneElse => "its "+someoneElse
+    }
+    assert(answer =="its vijay")
+
+    //sequence
+    def listMatcher(list:Any):String = {
+      list match{
+        case List(_,2,_) => "the list has a 2"
+        case List(0,_*) => "starts with 0"
+        case _ => "we don't care"
+      }
+    }
+    assert(listMatcher(List(1,2,3)) == "the list has a 2")
+    assert(listMatcher(List(0,4,6,7,8)) == "starts with 0")
+
+    //tuple
+    val myTuple = (1,"1",Number(1))
+    val tupleMatch: String = myTuple match {
+      case (_, "1", expr: Expr) => "1" //not the type match here
+      case (2, "1", Number(1)) => "2"
+      case _ => "0"
+    }
+    assert(tupleMatch == "1")
+
+    //type
+    def generalSize(x: Any) = x match {
+      case s: String => s.length
+      case m: Map[_, _] => m.size
+      case _ => -1
+    }
+    assert(generalSize("rekha") == 5)
+    assert(generalSize(Map("1"->1,"2"->2)) == 2)
+  }
+
+  test("pattern guard"){
+    val arr = Array(1,2,4)
+    val matched: String = arr match {
+      case Array(x,_*) if (x == 1) => "1"
+      case Array(x,_*) if (x == 2) => "2"
+      case _ => "-1"
+    }
+    assert (matched == "1")
+  }
+
+
+
 
 }
