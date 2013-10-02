06da185f333eed5e93df8eacd99c9957f7e2065d
vijayrc <vijayrc@outlook.com>
2013-10-02 17:40:06 -0400

    closing the sample play

--- play/todolist/app/controllers/Application.scala
+++ play/todolist/app/controllers/Application.scala
@@ -1,18 +1,34 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api.data.Forms._
+import play.api.data._
+import models.Task
 
 object Application extends Controller {
-  
+
+  val taskForm = Form("label" -> nonEmptyText)
+
   def index = Action {
-//    Ok(views.html.index("Your new application is ready."))
-    Ok("Hello Play")
+    Redirect(routes.Application.tasks)
   }
 
-  def tasks = TODO
-  def newTask = TODO
+  def tasks = Action {
+    Ok(views.html.index(Task.all, taskForm))
+  }
+
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all, errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
+
   def deleteTask(id:Long) = TODO
 
   
 }
+
