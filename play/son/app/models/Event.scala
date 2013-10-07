f7c1985e045510156a02241670a16348787a382a
vijayrc <vijayrc@outlook.com>
2013-10-06 21:13:32 -0400

    wired up bootswatch, listing some dummy events

--- play/son/app/models/Event.scala
+++ play/son/app/models/Event.scala
@@ -1,10 +1,18 @@
 package models
 
-import org.joda.time.DateTime
+import java.text.SimpleDateFormat
+import java.util.Date
 
-case class Event (val name:String, title:String, html:String, time:DateTime)
+class Event (val name:String, val title:String,val location:String, val time:Date){
+
+}
 
 object Event{
   def save(event:Event) ={}
-  def all:List[Event] = {Nil}
+  def all:List[Event] = {
+    val df = new SimpleDateFormat("dd/MM/yyyy");
+    val event1 = new Event("birth","Day of Entry","Vellore, TN, India",df.parse("21/11/2011"))
+    val event2 = new Event("naming","Naming Ceremony","Vellore, TN, India",df.parse("11/12/2011"))
+    event1::event2::Nil
+  }
 }
