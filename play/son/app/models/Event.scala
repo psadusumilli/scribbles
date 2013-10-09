3978e629058b504f1d6de88f07ccede4e7450d69
vijayrc <vijayrc@outlook.com>
2013-10-08 20:01:50 -0400

    some css work

--- play/son/app/models/Event.scala
+++ play/son/app/models/Event.scala
@@ -5,14 +5,16 @@ import java.util.Date
 
 class Event (val name:String, val title:String,val location:String, val time:Date){
     private var html = ""
+    private val df = new SimpleDateFormat("dd MMM yyyy")
     def addContent(h:String) {html = h}
     def content:String= {html}
+    def date = df.format(time)
 }
 
 object Event{
   def save(event:Event) ={}
   def all:List[Event] = {
-    val df = new SimpleDateFormat("dd/MM/yyyy");
+    val df = new SimpleDateFormat("dd/MM/yyyy")
     val event1 = new Event("birth","Day of Entry","Vellore, TN, India",df.parse("21/11/2011"))
     val event2 = new Event("naming","Naming Ceremony","Vellore, TN, India",df.parse("11/12/2011"))
     event1::event2::Nil
