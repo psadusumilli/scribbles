24105b982ac0d128a7ba52c8de52d4fd3a6f65c0
vijayrc <vijayrc@outlook.com>
2013-10-26 19:13:26 -0400

    hello hadoop with wordcount

---
+++ hadoop/wordcount/src/main/java/com/vijayrc/hadoop/wordcount/MyMapper.java
@@ -0,0 +1,17 @@
+package com.vijayrc.hadoop.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+       String line = value.toString();
+        for (String word : line.split("[\\s]"))
+            context.write(new Text(word),new IntWritable(1));
+    }
+}
