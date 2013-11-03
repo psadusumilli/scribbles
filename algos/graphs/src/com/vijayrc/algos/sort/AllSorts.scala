39a990852b195f78fa343f24628e21317f43bc9c
vijayrc <vijayrc@outlook.com>
2013-11-03 14:42:28 -0500

    moving all sorts to a single file

--- algos/graphs/src/com/vijayrc/algos/sort/AllSorts.scala
+++ algos/graphs/src/com/vijayrc/algos/sort/AllSorts.scala
@@ -1,5 +1,30 @@
 package com.vijayrc.algos.sort
 
+class AllSorts {}
+
+/**
+* move from left (i ->+ sz)
+* 'select' the smallest from remaining right subset[(i+1) -> sz]
+* swap it with the picked left i-th element
+* sorted subset grows from left to right
+* O(1) extra space
+* O(n2) comparisons
+* O(n) swaps
+* not adaptive for already sorted sets
+*/
+class SelectionSort extends Sort{
+  def on(items: Array[Value]): Array[Value] = {
+    for(i <- 0 until items.size){
+      var smallestIndex = i
+      for(j <- i+1 until items.size){
+        if(items(j) < items(smallestIndex))
+          smallestIndex = j
+      }
+      swap(i,smallestIndex,items)
+    }
+    items
+  }
+}
 /**
  * move from left to right (i->sz)
  * pick the first element from right subset (i+1->sz)
@@ -12,7 +37,6 @@ package com.vijayrc.algos.sort
  * adaptive for already sorted sets, better than 'selection' sort
  */
 class InsertionSort extends Sort{
-
   def on(items: Array[Value]): Array[Value] = {
     for(i <- 0 until items.size){
       for(j <- i to 1 by -1){ // starting with i, decreasing by 1 upto `1
@@ -23,3 +47,4 @@ class InsertionSort extends Sort{
     items
   }
 }
+
