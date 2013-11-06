0baf3732f41a9079476fef16e1217cefeb402b22
vijayrc <vijayrc@outlook.com>
2013-11-06 09:52:53 -0500

    cosmetic changes

--- algos/graphs/src/com/vijayrc/algos/probs/Dijkstra2Stack.scala
+++ algos/graphs/src/com/vijayrc/algos/probs/Dijkstra2Stack.scala
@@ -3,9 +3,38 @@ package com.vijayrc.algos.probs
 import com.vijayrc.algos.stack.{StackArray, Stack}
 
 class Dijkstra2Stack {
-  private val valueStack:Stack = new StackArray(4)
-  private val operatorStack:Stack = new StackArray(4)
+  private val values:Stack = new StackArray(4)
+  private val operators:Stack = new StackArray(4)
 
+  def evaluate(expr:String):Int = {
+    var flag = false //multi-digit numbers flag, is previous character digit
+    expr.foreach(c =>
+      if(isDigit(c)){
+        if(flag) values.push(values.pop()+""+c)
+        else values.push(c)
+        flag = true
+      }
+      else if(isOperator(c)) {flag = false; operators.push(c)}
+      else if(c == '(') flag = false
+      else if(c == ')'){
+        flag = false
+        val a = values.pop()
+        val b = values.pop()
+        val op = operators.pop()
+        values.push(doOps(a,b,op))
+      }else println("unknown char: "+c)
+    )
+    values.pop().toString.toInt
+  }
+
+  private def doOps(a:Any, b:Any, op:Any):Int = {
+    op match{
+        case '*' => intOf(b) * intOf(a)
+        case '+' => intOf(b) + intOf(a)
+        case '-' => intOf(b) - intOf(a)
+        case '/' => intOf(b) / intOf(a)
+    }
+  }
   private def isDigit(c: Char): Boolean = {
     var flag = false
     (0 to 9).foreach(i => if(c == i.toString.charAt(0)) flag = true)
@@ -16,36 +45,8 @@ class Dijkstra2Stack {
     Array('*','+','/','-').foreach(i => if(c == i) flag = true)
     flag
   }
-  def evaluate(expr:String):Int = {
-    var flag = false //multi-digit numbers flag, is previous character digit
-    expr.foreach(c =>
-      if(isDigit(c)){
-        if(flag) valueStack.push(valueStack.pop()+""+c)
-        else valueStack.push(c)
-        flag = true
-      }
-      else if(isOperator(c)) {flag = false; operatorStack.push(c)}
-      else if(c == '('){flag = false}
-      else if(c == ')'){
-        flag = false
-        val a = valueStack.pop()
-        val b = valueStack.pop()
-        val op = operatorStack.pop()
-        valueStack.push(doOps(a,b,op))
-      }else{
-        println("unknown char: "+c)
-      }
-    )
-    valueStack.pop().toString.toInt
-  }
-
-  private def doOps(a:Any, b:Any, op:Any):Int = {
-    op match{
-        case '*' => b.toString.toInt * a.toString.toInt
-        case '+' => b.toString.toInt + a.toString.toInt
-        case '-' => b.toString.toInt - a.toString.toInt
-        case '/' => b.toString.toInt / a.toString.toInt
-    }
+  private def intOf(any:Any):Int = {
+    any.toString.toInt
   }
 
 }
