a0544d5fbe423c734e2955660e8ecb9c6c9b42a4
vijayrc <vijayrc@outlook.com>
2013-12-14 15:51:32 -0500

    got in Postings to track occurrences

--- info-retrieval/src/com/vijayrc/info/boolean/InvertedIndex.scala
+++ info-retrieval/src/com/vijayrc/info/boolean/InvertedIndex.scala
@@ -6,38 +6,57 @@ import scala.collection.mutable
 import scala.collection.immutable.TreeMap
 
 /**
- *  CLASS CODE
+ * Processes documents then saves map of words->list of Postings(file,occurrences)
  */
 class InvertedIndex {
-  var dict = TreeMap[String,mutable.SortedSet[String]]()
+  var treeMap = TreeMap[String,mutable.SortedSet[Posting]]()
 
   def processDoc(filePath:String, fileId:String){
     val lines: Iterator[String] = Source.fromFile(filePath).getLines()
     lines.foreach(line => processLine(line,fileId))
-
   }
 
   def processLine(line: String, fileId:String){
-    line.split("\\s").foreach(token => processToken(token, fileId))
+    line.split("\\s").foreach(token => processToken(token.replaceAll("[.|,|:|;]",""), fileId))
   }
 
   def processToken(token: String, fileId:String){
-    val list = mutable.SortedSet[String]()
-    if(!dict.contains(token)) {dict += (token -> list)}
-    dict(token).+=(fileId)
+    if(!treeMap.contains(token))
+      treeMap += (token -> mutable.SortedSet[Posting]())
+
+    val postings = treeMap(token)
+    val postingsByFile = postings.filter(p => p.file.equals(fileId))
+    if(postingsByFile.isEmpty)
+      treeMap(token).+=(new Posting(fileId))
+    else
+      postingsByFile.foreach(p => p.increment)
   }
 
   def print(){
     var str = ""
-    dict.keys.foreach(key => {str+=key+"["; dict(key).foreach(doc => str += doc+"|"); str+="]"})
+    treeMap.keys.foreach(key => {str+=key+"["; treeMap(key).foreach(posting => str += posting+"|"); str+="]\n"})
     println(str)
   }
+
+}
+
+class Posting(val file:String) extends Ordered[Posting]{
+  var occurrences = 1
+  def increment{occurrences+=1}
+  override def equals(obj: scala.Any): Boolean = file.equals(obj.asInstanceOf[Posting].file)
+  override def toString: String = file+","+occurrences
+  override def compare(that: Posting): Int = file.compareTo(that.file)
 }
 
 
-/**
- * TEST CODE
- */
+
+
+
+
+
+
+
+
 class InvertedIndexTest extends FunSuite{
   val index = new InvertedIndex
 
