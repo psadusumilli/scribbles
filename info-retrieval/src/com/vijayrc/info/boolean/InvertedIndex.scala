c39fe1ce8d6eef487b87b9a842fa5163a6879ea2
vijayrc <vijayrc@outlook.com>
2013-12-14 22:28:29 -0500

    refactored and brought in postingset

--- info-retrieval/src/com/vijayrc/info/boolean/InvertedIndex.scala
+++ info-retrieval/src/com/vijayrc/info/boolean/InvertedIndex.scala
@@ -6,46 +6,69 @@ import scala.collection.mutable
 import scala.collection.immutable.TreeMap
 
 /**
- * Processes documents then saves map of words->list of Postings(file,occurrences)
+ * reads documents then saves map of terms->posting-set(posting(file,occurrences))
  */
 class InvertedIndex {
-  var treeMap = TreeMap[String,mutable.SortedSet[Posting]]()
+  var treeMap = TreeMap[String,PostingSet]()
 
   def processDoc(filePath:String, fileId:String){
     val lines: Iterator[String] = Source.fromFile(filePath).getLines()
     lines.foreach(line => {line.split("\\s").foreach(token => processToken(token.replaceAll("[.|,|:|;]",""), fileId))})
   }
   def processToken(token: String, fileId:String){
-    if(!treeMap.contains(token)) treeMap += (token -> mutable.SortedSet[Posting]())
-    val postings = treeMap(token)
-    val postingsByFile = postings.filter(p => p.file.equals(fileId))
-    if(postingsByFile.isEmpty) treeMap(token).+=(new Posting(fileId))
-    else postingsByFile.foreach(p => p.increment)
+    if(!treeMap.contains(token)) treeMap += (token -> new PostingSet())
+    treeMap(token).add(fileId)
   }
   def print(){
     var str = ""
-    treeMap.keys.foreach(key => {str+=key+"["; treeMap(key).foreach(posting => str += posting+"|"); str+="]\n"})
+    treeMap.keys.foreach(term => {str+=term+"["+ treeMap(term).toString +"]\n"})
     println(str)
+  }
+  def findDocsFor(ands:List[String]):List[String]={
+     val foundPostings = mutable.MutableList[mutable.SortedSet[Posting]]()
+
+     ands
+  }
+  private def intersect(set1:Set[Posting],set2:Set[Posting]):Set[Posting] = {
+    if (set1.size >= set2.size) set1.intersect(set2)
+    else set2.intersect(set1)
   }
 }
 
-class Posting(val file:String) extends Ordered[Posting]{
+/**
+ * api with fileId and occurrence count for a given term
+ */
+class Posting(val fileId:String) extends Ordered[Posting]{
   var occurrences = 1
-  def increment{occurrences+=1}
-  override def equals(obj: scala.Any): Boolean = file.equals(obj.asInstanceOf[Posting].file)
-  override def toString: String = file+","+occurrences
-  override def compare(that: Posting): Int = file.compareTo(that.file)
+  def increment(){occurrences+=1}
+  override def equals(obj: scala.Any): Boolean = fileId.equals(obj.asInstanceOf[Posting].fileId)
+  override def toString: String = fileId+","+occurrences
+  override def compare(that: Posting): Int = fileId.compareTo(that.fileId)
 }
 
+/**
+ * set of all postings for a given term
+ */
+class PostingSet extends Ordered[PostingSet]{
+  val set = mutable.SortedSet[Posting]()
 
+  def add(fileId:String){
+    val byFile = set.filter(p => p.fileId.equals(fileId))
+    if(byFile.isEmpty) set.+=(new Posting(fileId))
+    else byFile.foreach(p => p.increment())
+  }
+  def totalTermOccurrence:Int = {
+    var count = 0
+    set.foreach(p=>count+=p.occurrences)
+    count
+  }
+  override def compare(that: PostingSet): Int = this.set.size.compare(that.set.size)
+  override def toString: String = {var str=""; set.foreach(p => str += p.toString+"|");str}
+}
 
-
-
-
-
-
-
-
+/**
+ *  test it out
+ */
 class InvertedIndexTest extends FunSuite{
   val index = new InvertedIndex
 
@@ -53,5 +76,6 @@ class InvertedIndexTest extends FunSuite{
     index.processDoc(getClass.getResource("/boolean/file1.txt").getFile,"f1")
     index.processDoc(getClass.getResource("/boolean/file2.txt").getFile,"f2")
     index.print()
+    index.findDocsFor(List("South","episode"))
   }
 }
