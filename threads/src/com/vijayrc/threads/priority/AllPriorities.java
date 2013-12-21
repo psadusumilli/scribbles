355f3402725724e403660e174944c7d1b5abac94
vijayrc <vijayrc@outlook.com>
2013-12-21 11:53:40 -0500

    done with basic wait/notify

--- threads/src/com/vijayrc/threads/priority/AllPriorities.java
+++ threads/src/com/vijayrc/threads/priority/AllPriorities.java
@@ -5,8 +5,10 @@ import static com.vijayrc.threads.util.Printer.log2;
 
 public class AllPriorities {
     public static class Pi extends Thread{
-        boolean negative = true;
-        double value;
+        private boolean negative = true;
+        private boolean shouldYield = false;
+        private double value;
+
         public Pi(String name) {
             super(name);
         }
@@ -16,37 +18,46 @@ public class AllPriorities {
                 else value += (1.0 / i);
                 negative = !negative;
                 log2(getName() + "|");
+                if(shouldYield) Thread.yield();
             }
             value += 1.0;
             value *= 4.0;
             log("\n"+getName()+"|"+"finished calculating pi="+value);
+        }
+        public void relent(){
+            shouldYield = true;
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         withoutPriorities();
         withPriorities();
+        withPrioritiesAndYield();
     }
-
     /**
      * mixed p1|p2|p1|p2|p2|p1
      */
     private static void withoutPriorities() {
-        Pi p1 = new Pi("p1");
-        Pi p2 = new Pi("p2");
-        p1.start();
-        p2.start();
+        Pi p1 = new Pi("p1"); Pi p2 = new Pi("p2");
+        p1.start(); p2.start();
     }
     /**
-     * some order repetitively,
+     * some order repetitively p1|p1|p1|p2|p2..
      * not guaranteed owing to time-slicing of processor
      */
     private static void withPriorities() {
-        Pi p1 = new Pi("p1");
-        Pi p2 = new Pi("p2");
-        p1.setPriority(9);
-        p2.setPriority(1);
-        p1.start();
-        p2.start();
+        Pi p1 = new Pi("p1"); Pi p2 = new Pi("p2");
+        p1.setPriority(9); p2.setPriority(1);
+        p1.start(); p2.start();
+    }
+    /**
+     * p1 yields to p2 in every loop
+     * again not guaranteed owing to time-slicing
+     */
+    private static void withPrioritiesAndYield() {
+        Pi p1 = new Pi("p1"); Pi p2 = new Pi("p2");
+        p1.setPriority(9); p2.setPriority(1);
+        p1.relent();
+        p1.start(); p2.start();
     }
 }
