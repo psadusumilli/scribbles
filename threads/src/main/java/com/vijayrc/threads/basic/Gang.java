2c47b13a8e484701dbed967dd7b31b2a8b421360
vijayrc <vijayrc@outlook.com>
2013-12-23 13:14:07 -0500

    package shifting

---
+++ threads/src/main/java/com/vijayrc/threads/basic/Gang.java
@@ -0,0 +1,45 @@
+package com.vijayrc.threads.basic;
+
+import static com.vijayrc.threads.util.Printer.log;
+
+public class Gang {
+    /**
+     * each gang member waits on a interrupt call from his boss
+     * the lock is the string constant "phone"
+     * since they execute in a synchronised context, they respond one-by-one
+     * the gang is heirarchical, g1->g2, g1.interrupt would call everyone recursively
+     */
+    public static class Member extends Thread{
+        public Member(ThreadGroup group,String name) {super(group,name);}
+
+        @Override
+        public void run() {
+            synchronized ("phone"){
+                log(getName()+"|member=>boss did not call, I can sleep around");
+                try {
+                    "phone".wait();
+                } catch (InterruptedException e) {
+                    log(getName()+"|member=> shit..boss called, must inform the next guy");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        ThreadGroup mainGroup = Thread.currentThread().getThreadGroup();
+        log("main thread's group|"+mainGroup);
+        log("main thread's group's parent|"+mainGroup.getParent());
+
+        ThreadGroup g1 = new ThreadGroup("G1");
+        ThreadGroup g2 = new ThreadGroup(g1,"G2");
+
+        Thread t1 = new Member(g1,"t1");t1.start();
+        Thread t2 = new Member(g1,"t2");t2.start();
+        Thread t3 = new Member(g2,"t3");t3.start();
+        Thread t4 = new Member(g2,"t4");t4.start();
+
+        g1.list();
+        log("g1 boss calling");
+        g1.interrupt();
+    }
+}
---
+++ threads/src/main/java/com/vijayrc/threads/basic/Priorities.java
@@ -0,0 +1,63 @@
+package com.vijayrc.threads.basic;
+
+import static com.vijayrc.threads.util.Printer.log;
+import static com.vijayrc.threads.util.Printer.log2;
+
+public class Priorities {
+    public static class Pi extends Thread{
+        private boolean negative = true;
+        private boolean shouldYield = false;
+        private double value;
+
+        public Pi(String name) {
+            super(name);
+        }
+        public void run() {
+            for (int i = 3; i < 1000; i += 2) {
+                if (negative) value -= (1.0 / i);
+                else value += (1.0 / i);
+                negative = !negative;
+                log2(getName() + "|");
+                if(shouldYield) Thread.yield();
+            }
+            value += 1.0;
+            value *= 4.0;
+            log("\n"+getName()+"|"+"finished calculating pi="+value);
+        }
+        public void relent(){
+            shouldYield = true;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        withoutPriorities();
+        withPriorities();
+        withPrioritiesAndYield();
+    }
+    /**
+     * mixed p1|p2|p1|p2|p2|p1
+     */
+    private static void withoutPriorities() {
+        Pi p1 = new Pi("p1"); Pi p2 = new Pi("p2");
+        p1.start(); p2.start();
+    }
+    /**
+     * some order repetitively p1|p1|p1|p2|p2..
+     * not guaranteed owing to time-slicing of processor
+     */
+    private static void withPriorities() {
+        Pi p1 = new Pi("p1"); Pi p2 = new Pi("p2");
+        p1.setPriority(9); p2.setPriority(1);
+        p1.start(); p2.start();
+    }
+    /**
+     * p1 yields to p2 in every loop
+     * again not guaranteed owing to time-slicing
+     */
+    private static void withPrioritiesAndYield() {
+        Pi p1 = new Pi("p1"); Pi p2 = new Pi("p2");
+        p1.setPriority(9); p2.setPriority(1);
+        p1.relent();
+        p1.start(); p2.start();
+    }
+}
