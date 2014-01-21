44729a79cb534336ecc9105a2a56dab8721072d8
vijayrc <vijayrc@outlook.com>
2014-01-20 22:08:10 -0500

    modular routing works

--- angularjs/src/main/webapp/js/try.js
+++ angularjs/src/main/webapp/js/try.js
@@ -1,4 +1,4 @@
-var module1 = angular.module('m1',[]);
+var module1 = angular.module('m1',['ngRoute']);
 var controllers = {};
 
 controllers.controller1 = function($scope){
@@ -8,5 +8,19 @@ controllers.controller1 = function($scope){
        {name:'kenny',id:3},
        {name:'kyle',id:4}]
 };
-
+controllers.controller2 = function($scope){
+  $scope.girls=[
+       {name:'bebe',id:1},
+       {name:'wendy',id:2},
+       {name:'shelly',id:3},
+       {name:'red',id:4}]
+};
 module1.controller(controllers);
+
+module1.config(function($routeProvider){
+    $routeProvider
+        .when('/all',{controller:'controller1',templateUrl:'all.html'})
+        .when('/boys',{controller:'controller1',templateUrl:'boys.html'})
+        .when('/girls',{controller:'controller2',templateUrl:'girls.html'})
+        .otherwise({redirectTo:'/all'});
+});
