d3170136535352472e176968d8a00a5031171383
vijayrc <vijayrc@outlook.com>
2014-01-26 16:19:34 -0500

    more work

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,8 +1,6 @@
 title("objects")
 
-
-
-//prototypes
+/*prototypes_________________________________________________________________________*/
 /* creates a new object which prototype is person */
 var p1 = {}
 console.log(Object.getPrototypeOf(p1))
@@ -14,7 +12,8 @@ var zack1 = Object.create(person1)
 console.log('zack1 kind='+zack1.kind+" & its prototype 'person1'=") // => ‘person1’
 console.log(Object.getPrototypeOf(zack1))
 
-//__proto__
+/*__proto_____________________________________________________________________________*/
+/*updating obj property will not affect its proto, but proto will affect obj property*/
 var alien = {  kind: 'alien'}
 var person = {  kind: 'person'}
 var zack = {}
@@ -22,28 +21,65 @@ zack.__proto__ = alien
 console.log('zack kind='+zack.kind) //=> ‘alien’
 zack.__proto__ = person
 console.log('zack kind='+zack.kind) //=> ‘person’
-
-/**updating obj property will not affect its proto, but proto will affect obj property*/
 zack.kind='zack'
 console.log('zack kind='+zack.kind) //=> ‘zack’
 console.log('person kind='+person.kind) //=> ‘person’
 
-//delete
+/*delete_____________________________________________________________________________*/
+/*deleting inherited properties not possible,can be done on the owning parent only */
 console.log("delete")
 var obj = { x: 1, y: 2, z:{ z1:3, z2:4 } }; 
 delete obj.x; 
 console.log("x" in obj); // => false
 delete obj.z.z2
 console.log(obj)
-//deleting inherited properties not possible,can be done on the owning parent only
+
 var parent  = {a:1, b:2}
 obj.__proto__= parent
 console.log(Object.getPrototypeOf(obj))
 delete(obj.a)
-console.log(parent)
-console.log(obj.a)
+console.log(parent);console.log(obj.a)
 delete(parent.a)
 console.log(obj.a)
+
+delete Object.prototype;// not possible
+var x = 1; 
+delete this.x; //not possible
+function f() {}
+delete this.f; //not possible
+
+/*obj methods_____________________________________________________________________________*/
+var obj={name:'o1',age:3}
+obj.say = function(){return this.age} //=>this refers to scope of obj
+obj.print = function(){console.log("obj method:name="+this.name+"|age="+this.age)}
+console.log("obj method:"+obj.say())
+obj.print()
+
+/*getters|setters - 2 point to polar coordinates____________________________________________________*/
+var point = {
+	x : 1, 
+	y : 2,
+	get r() {return Math.sqrt(this.x*this.x+this.y*this.y);},
+	set r(newvalue) {
+		var oldvalue = Math.sqrt(this.x*this.x + this.y*this.y);
+		var ratio = newvalue/oldvalue;
+		this.x *= ratio;
+		this.y *= ratio;
+	},
+	get theta() { return Math.atan2(this.y, this.x); }//readonly
+}
+point.print = function(){console.log("point: x="+this.x+"|y="+this.y)}	
+point.print()
+console.log("point.r="+point.r)
+point.r = 4
+point.print()
+console.log("point.theta="+point.theta)
+
+
+
+
+
+
 
 
 
