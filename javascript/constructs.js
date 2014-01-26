c4b0c9f83498ca59ac96ae22595146a310156603
vijayrc <vijayrc@outlook.com>
2014-01-25 19:55:53 -0500

    basics again js

--- javascript/constructs.js
+++ javascript/constructs.js
@@ -10,3 +10,102 @@ if (a == 4)
 else
 	a = a + 1;
 console.log("a="+a+"|b="+b)
+
+//for loop
+console.log("for-loop-1")
+for(var i = 0, j=10; i < 10; i++,j--) 
+	console.log(i*j);
+
+console.log("for-loop-2")
+var a = [1,2,3,4]
+for(var p in a) 
+	console.log(a[p]);
+
+//while loop
+console.log("while-loop")
+var i=1
+while(i<=4){
+	console.log(i)
+	i++
+}
+
+//switch
+console.log("switch")
+var decide = function(x){
+	switch(x){
+		case 1: console.log("1");break;
+		case 2: console.log("2");break;	
+		case 3: console.log("3");break;	
+		default: console.log("X");break;
+	}
+}
+decide(2)
+decide(12)
+	
+//in-operator
+console.log("in-operator")
+var point = { x:1, y:1 }; 
+var a1= "x" in point // => true
+var a2 = "z" in point // => false
+var a3 = "toString" in point // => true: object inherits toString method
+console.log(a1+"|"+a2+"|"+a3)
+
+var data = [0,1,2]; 
+a1 = "0" in data // => true
+a2 = 1 in data // => true
+a3 = 3 in data // => false
+console.log(a1+"|"+a2+"|"+a3)
+
+var d = new Date(); 
+var t = d instanceof Date;	
+console.log("instance of ="+t)
+
+//eval
+console.log("eval")
+var geval = eval; // Using another name does a global eval
+var x = "global", y = "global";
+
+function f() { 
+	var x = "local"; 
+	eval("x += '-changed';"); 
+	return x; 
+}
+function g() { 
+	var y = "local"; 
+	geval("y += '-changed';"); 
+	return y; 
+}
+console.log(f()+"|"+x); // Local variable changed: prints "local-changed global":
+console.log(g()+"|"+y); // Global variable changed: prints "local global-changed":
+
+
+//delete
+console.log("delete")
+var o = { x: 1, y: 2}; 
+delete o.x; 
+console.log("x" in o); // => false
+
+var a = [1,2,3]; 
+delete a[2]; 
+console.log(a.length+"|"+a[2]) // => 3|undefined: 
+
+
+//try-catch-throw
+console.log("error")
+function minus(x){
+	try{
+		if(x<0) throw new Error("x is -ve")
+	}catch(e){
+		console.log(e.toString())
+	}finally{
+		console.log("all done")
+	}
+}
+minus(-2)
+
+
+
+
+
+
+
--- javascript/functions.js
+++ javascript/functions.js
@@ -12,3 +12,36 @@ console.log("square:"+square(plus1(y))) //=>16
 var obj={name:'o1'}
 obj.say = function(){return this.name} //=>this refers to scope of obj
 console.log("obj method:"+obj.say())
+
+/*JavaScript’s function scope means that all variables declared within a function are visible
+throughout the body of the function. Curiously, this means that variables are even
+visible before they are declared. This feature of JavaScript is informally known as hoisting:
+JavaScript code behaves as if all variable declarations in a function (but not any
+associated assignments) are “hoisted” to the top of the function.*/
+var g1 = "g1"
+var g2 = "g2"
+function scopetest() {
+	var i = 0;
+	console.log("g1="+g1+"|g2="+g2)//=> g1=undefined|g2=g2  
+	//so global is overridden by local g1 declaration hoisted to top of function, global g2 is not touched
+	if (true) {
+		var j = 21; 
+		var g1 = "local-g1"
+		for(var k=0; k < 2; k++) { 
+			console.log("k inside loop="+k); //=>0-2
+		}
+		console.log("k outside loop="+k); //=>2
+	}
+	console.log("outside 'if' block j="+j); //=>21
+	console.log("g1="+g1+"|g2="+g2) //g1=local-g1|g2=g2
+}
+scopetest()
+
+//global variables deleting
+var truevar = 1; // A properly declared global variable, nondeletable.
+fakevar = 2; // Creates a deletable property of the global object.
+this.fakevar2 = 3; // This does the same thing.
+delete truevar // => false: variable not deleted
+delete fakevar // => true: variable deleted
+delete this.fakevar2 // => true: variable deleted
+
