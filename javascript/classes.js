4b80d4077cdbbe8674e069f6594aa37122ce804a
vijayrc <vijayrc@outlook.com>
2014-01-29 21:01:10 -0500

    more concrete added to classes

--- javascript/classes.js
+++ javascript/classes.js
@@ -3,16 +3,23 @@ title("classes")
 M = function(x,y){
 	var v1 = 5
 	this.v2 = 10
-
 	this.m0 =  function(){return x+y}
 	this.m1 = function(){return v1*(x+y)}
 	this.m2 = function(){return this.v2*(x+y)} //this.v2 ('this' reqd)
+	console.log("M called: v1="+v1+"|v2="+this.v2)
 }
+M.v3 = 9
+M.m3 = function(){return this.v3}//x, y, v2 cannot come here (m and M are not related)
 
-M.prototype.p1 = function() {return this.v2*(x+y)};
+var n = M.prototype
+n.v4= 15
+n.m4 = function() {return this.v4*this.v2; } //local x, y, v1 cannot be reached, v3=undefined, belongs to M object
 
-var m = new M(1,2)
 
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+var m = new M(1,2) //=>M called: v1=5|v2=10
 //-----------------------------------------------------------------------
 console.log("v1|local variable="+m.v1) //=>undefined
 //-----------------------------------------------------------------------
@@ -24,8 +31,22 @@ console.log("m1|method call with constructor params and local variable="+m.m1())
 //-----------------------------------------------------------------------
 console.log("m2|method call with constructor params and object property="+m.m2())//=>20 v2 is bound to the new object 
 //-----------------------------------------------------------------------
-
+console.log("m|function object=")
+console.log(m)
 //-----------------------------------------------------------------------
+console.log("m|Object.getPrototypeOf(m)=")
+console.log(Object.getPrototypeOf(m))
+console.log("M|M.prototype=")
+console.log(M.prototype)
+console.log("m4|inherited method call with owned and inherited object properties="+m.m4())//=>150
+//-----------------------------------------------------------------------
+console.log("M.m3|method call on M object="+M.m3())
+
+
+
+
+
+
 
 //-----------------------------------------------------------------------
 
