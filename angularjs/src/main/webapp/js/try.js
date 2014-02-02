56aa92ea7ed825d48f019c679cc8e51b9646e69e
vijayrc <vijayrc@outlook.com>
2014-02-02 10:27:01 -0500

    dependency working

--- angularjs/src/main/webapp/js/try.js
+++ angularjs/src/main/webapp/js/try.js
@@ -1,6 +1,14 @@
 var module1 = angular.module('m1',['ngRoute']);
 var controllers = {};
 
+module1.constant("title","SouthPark Elementary")
+
+var GirlsRepo = function(){
+    this.all = function(){return [{name:'bebe',id:1},{name:'wendy',id:2},{name:'shelly',id:3},{name:'red',id:4}]};
+}
+module1.value("girlsRepo", new GirlsRepo());//no dependency on anything else
+
+
 controllers.controller1 = function($scope){
   $scope.boys=[
        {name:'stan',id:1},
@@ -12,12 +20,9 @@ controllers.controller1 = function($scope){
      $scope.boys.push({name:$scope.newboy.name,id:$scope.newboy.id});
   };
 };
-controllers.controller2 = function($scope){
-  $scope.girls=[
-       {name:'bebe',id:1},
-       {name:'wendy',id:2},
-       {name:'shelly',id:3},
-       {name:'red',id:4}]
+controllers.controller2 = function($scope,girlsRepo,title){
+  $scope.girls = girlsRepo.all();
+  $scope.title = title;
 };
 controllers.controller3 = function($scope){
   $scope.s0="D";
@@ -33,3 +38,7 @@ module1.config(function($routeProvider){
         .when('/scope',{controller:'controller3',templateUrl:'scope.html'})
         .otherwise({redirectTo:'/all'});
 });
+
+
+
+
