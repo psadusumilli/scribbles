c490949cf0b9795acc7df0626156eba5d231ee9a
vijayrc <vijayrc@outlook.com>
2014-02-06 22:17:01 -0500

    unapply example

---
+++ scala/src/com/vijayrc/extractor/Extract.scala
@@ -0,0 +1,10 @@
+package com.vijayrc.extractor
+
+object Twice {
+  def apply(x: Int): Int = x * 2
+  def unapply(z: Int): Option[Int] = if (z%2 == 0) Some(z/2) else None
+}
+object TwiceTest extends App {
+  val x = Twice(21)
+  x match { case Twice(n) => Console.println(n) } // prints 21
+}
