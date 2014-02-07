48f415def43dc2027fb8bc3574d57b7330488653
vijayrc <vijayrc@outlook.com>
2014-02-07 17:08:03 -0500

    implicit params done

--- scala/src/com/vijayrc/impliciter/Test1.scala
+++ scala/src/com/vijayrc/impliciter/Test1.scala
@@ -38,3 +38,23 @@ object Test3 extends App{
   assert(4.4 == euro.add(dollar))
 }
 
+/*params */
+class PreferredPrompt(val preference: String)
+class PreferredDrink(val preference: String)
+
+object Greeter {
+
+  def greet(name: String)(implicit prompt: PreferredPrompt, drink: PreferredDrink) {
+    println("Welcome, "+ name +". The system is ready.")
+    print("But while you work, ")
+    println("why not enjoy a cup of "+ drink.preference +"?")
+    println(prompt.preference)
+  }
+}
+object Test4 extends App{
+  implicit val prompt = new PreferredPrompt("Yes, master> ")
+  implicit val drink = new PreferredDrink("tea")
+
+  Greeter.greet("Joe")
+
+}
