db7266ddf8a4bc188abab6f8ce841df7e7c03167
vijayrc <vijayrc@outlook.com>
2014-02-11 19:34:03 -0500

    made config.json write work

--- akka/src/main/scala/com/vijayrc/akka/Greet.scala
+++ akka/src/main/scala/com/vijayrc/akka/Greet.scala
@@ -1,7 +1,9 @@
 package com.vijayrc.akka
-import akka.actor.Actor
-import akka.actor.Props
+import akka.actor.{ActorSystem, Actor, Props}
 import akka.Main
+import scala.io.Source
+import java.nio.file.Files
+import java.io.{File, FileWriter}
 
 /*
 * creates an actor2, tells it to print "hello" via Message case objects (immutable)
@@ -18,8 +20,15 @@ class Actor2 extends Actor {
 }
 class Actor1 extends Actor {
   override def preStart(): Unit = {
+    settings()
     val greeter = context.actorOf(Props[Actor2], "actor2")
     greeter ! Message.Greet
+  }
+  def settings() {
+    val f = new File("config.json")
+    if(f.exists()) return
+    val fw = new FileWriter(f)
+    try fw.write(context.system.settings.toString()) finally {fw.flush(); fw.close()}
   }
   def receive = {
     case Message.Done ⇒ println("actor1: thanks actor2");context.stop(self)
