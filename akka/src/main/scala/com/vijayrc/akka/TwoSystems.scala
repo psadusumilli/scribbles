2c8aef8aecc0ca2c9028989ca49f5a271315facf
vijayrc <vijayrc@outlook.com>
2014-02-12 05:14:29 -0500

    dependency injection/factory creation of actor working

--- akka/src/main/scala/com/vijayrc/akka/TwoSystems.scala
+++ akka/src/main/scala/com/vijayrc/akka/TwoSystems.scala
@@ -1,27 +1,39 @@
 package com.vijayrc.akka
 
-import akka.actor.{Props, ActorSystem, Actor}
+import akka.actor._
 import akka.event.Logging
 
+class DummyContext(val actorRef:ActorRef){
+  def actorBean = actorRef
+}
+class DependencyInjector(applicationContext: AnyRef, beanName: String) extends IndirectActorProducer {
+  override def actorClass = classOf[Actor]
+  override def produce() = {
+    val peer = applicationContext.asInstanceOf[DummyContext].actorBean
+    new A2(peer)
+  }
+}
 class A1 extends Actor{
   val log = Logging(context.system, this)
 
   def receive = {
-    case x:String => log.info("actor1|"+x)
+    case x:String => log.info(x)
+    case y:Int => log.info(y.toString)
   }
   override def postStop() {
-    log.info("actor1|stopping")
+    log.info("stopping")
     super.postStop()
   }
 }
-class A2 extends Actor{
+class A2(val peer:ActorRef) extends Actor{
   val log = Logging(context.system, this)
 
   def receive = {
-    case x:String => log.info("actor2|"+x)
+    case x:String => log.info(x)
+    case y:Int => {peer ! y}
   }
   override def postStop() {
-    log.info("actor2|stopping")
+    log.info("stopping")
     super.postStop()
   }
 }
@@ -33,9 +45,13 @@ class TwoSystems {
     val system2 = ActorSystem.create("system2")
     try {
       val actor1 = system1.actorOf(Props[A1],"actor1")
-      val actor2 = system2.actorOf(Props[A2],"actor2")
+
+      val dummy = new DummyContext(actor1)
+      val actor2 = system2.actorOf(Props(classOf[DependencyInjector], dummy, "actor2"),"dummyBean")
+
       actor1 ! "msg1"
       actor2 ! "msg2"
+      actor2 ! 2
     }
     finally {
       system1.shutdown()
