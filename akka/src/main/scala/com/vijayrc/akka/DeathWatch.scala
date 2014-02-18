ea4e747237991ee5d74176e66f672ac09ec95a38
vijayrc <vijayrc@outlook.com>
2014-02-18 12:35:34 -0500

    graceful stop and forward

--- akka/src/main/scala/com/vijayrc/akka/DeathWatch.scala
+++ akka/src/main/scala/com/vijayrc/akka/DeathWatch.scala
@@ -3,7 +3,11 @@ package com.vijayrc.akka
 import akka.actor._
 import akka.actor.Terminated
 import akka.event.Logging
-
+import akka.pattern.gracefulStop
+import scala.concurrent.duration._
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
 
 /*WatchActor creates a child, kills, watches it die*/
 object DeathWatch {
@@ -16,7 +20,13 @@ object DeathWatch {
       context.watch(child)
     }
     def receive = {
-      case "kill" => {log.info("killing child"); context.stop(child) }
+      case "kill" => log.info("killing child"); context.stop(child)
+      case "poison" => log.info("poisoning child"); child ! PoisonPill
+      case "graceful" => {
+        log.info("graceful kill child")
+        val stop: Future[Boolean] = gracefulStop(child, 5 seconds)
+        stop.onComplete(x=> println(x))
+      }
       case Terminated(child) => {log.info("child is dead")}
     }
   }
@@ -24,7 +34,9 @@ object DeathWatch {
     val system =  ActorSystem.create("system1")
     try {
       val watcher = system.actorOf(Props[WatchActor], "watcher")
-      watcher ! "kill"
+      //watcher ! "kill"
+      //watcher ! "poison"
+      watcher ! "graceful"
       Thread.sleep(1000)
     }
     finally system.shutdown()
--- akka/src/main/scala/com/vijayrc/akka/TellAndAsk.scala
+++ akka/src/main/scala/com/vijayrc/akka/TellAndAsk.scala
@@ -9,17 +9,33 @@ import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
 
 object TellAndAsk {
+
+  class Actor0 extends Actor{
+    def receive = {
+      case "forward" => sender ! "replying to forwarded msg"
+    }
+  }
+
   /*will reply to actor2*/
   class Actor1 extends Actor{
+    var actor0:ActorSelection = null
+
+    override def preStart(){
+      actor0 = context.actorSelection("/user/a0")
+    }
     def sleep() = Thread.sleep(2000)
+
     def receive = {
       case "short-tell" => println("actor1|short-tell"); sender ! "reply from short-tell"
       case "long-tell" => println("actor1|long-tell"); sleep(); sender ! "reply from long-tell"
       case "short-ask" => println("actor1|short-ask"); sender ! "reply from short-ask"
       case "long-ask" => println("actor1|tell with a reply"); sleep(); sender ! "reply from long-ask"
+      case "forward" => println("actor1|forwarding")//TODO find forward
     }
   }
-  /*tells/asks actor1 and bind futures for the same */
+  /*tells/asks actor1 and bind futures for the same
+   *actor1 forwards msgs to actor0, which in turn replies to original sender actor2
+   */
   class Actor2 extends Actor{
     var actor1:ActorSelection = null
     val id = 1
@@ -28,6 +44,7 @@ object TellAndAsk {
     override def preStart(){
       actor1 = context.actorSelection("/user/a1")
     }
+
     def receive = {
       case "send-short-tell" => actor1 ! "short-tell"
       case "send-long-tell" => actor1 ! "long-tell"
@@ -37,6 +54,7 @@ object TellAndAsk {
       case "send-long-ask" =>
         val future = actor1.ask("long-ask")
         future.onSuccess{case x:String => println("future:"+x)}
+      case "forward" => actor1 ! "forward"
       case x:String => println("actor2|"+now+"|"+x)
     }
 
@@ -44,12 +62,14 @@ object TellAndAsk {
   def work(){
     val system = ActorSystem.create("system1")
     try {
+      val a0 = system.actorOf(Props[Actor1], "a0")
       val a1 = system.actorOf(Props[Actor1], "a1")
       val a2 = system.actorOf(Props[Actor2], "a2")
       a2 ! "send-short-tell"
       a2 ! "send-long-tell"
       a2 ! "send-short-ask"
       a2 ! "send-long-ask"
+      a2 ! "forward"
     }
     finally {
       Thread.sleep(8000)
