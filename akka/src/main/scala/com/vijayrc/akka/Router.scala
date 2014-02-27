bbb8584736cb34e75f0ae05331dd1e097e9dfdc2
vijayrc <vijayrc@outlook.com>
2014-02-27 12:34:44 -0500

    more routing stuff on akka

--- akka/src/main/scala/com/vijayrc/akka/Router.scala
+++ akka/src/main/scala/com/vijayrc/akka/Router.scala
@@ -1,7 +1,7 @@
 package com.vijayrc.akka
 
 import akka.actor._
-import akka.routing.RoundRobinRouter
+import akka.routing._
 
 class Routee extends Actor with ActorLogging{
   def receive = {
@@ -15,12 +15,24 @@ object Router {
       var receivers = List[ActorRef]()
       for (a <- 1 until 5)
         receivers = system.actorOf(Props[Routee], "routee-" + a) :: receivers
-      val router = system.actorOf(Props.empty.withRouter(RoundRobinRouter(routees = receivers)))
+
+      val resizer = DefaultResizer(lowerBound = 2, upperBound = 15)
+      val robin = system.actorOf(Props.empty.withRouter(RoundRobinRouter(routees = receivers)))
+      val mailBox = system.actorOf(Props.empty.withRouter(SmallestMailboxRouter(routees = receivers)))
+      val random = system.actorOf(Props.empty.withRouter(RandomRouter(routees = receivers)))
+      val broadcast = system.actorOf(Props.empty.withRouter(BroadcastRouter(routees = receivers)))
+      val grow = system.actorOf(Props[Routee].withRouter(RoundRobinRouter(resizer = Some(resizer))))
+      val router = grow
+
       for (i <- 1 until 100)
         router ! "msg-" + i
+      router ! Broadcast("all done?")
+      router ! Broadcast(PoisonPill) //stops routees and then router
+
+
     }
     finally {
-      Thread.sleep(4000)
+      Thread.sleep(2000)
       system.shutdown()
     }
   }
