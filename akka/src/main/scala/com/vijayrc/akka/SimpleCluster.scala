516b0e79e667575cf3de48ff9c1e0d630a0457d1
vijayrc <vijayrc@outlook.com>
2014-03-04 19:16:05 -0500

    going with cluster 2.10

--- akka/src/main/scala/com/vijayrc/akka/SimpleCluster.scala
+++ akka/src/main/scala/com/vijayrc/akka/SimpleCluster.scala
@@ -1,30 +1,22 @@
 package com.vijayrc.akka
 
 import akka.actor.{Props, ActorSystem, Actor, ActorLogging}
-//import akka.cluster.Cluster
-//import akka.cluster.ClusterEvent._
-import akka.actor.{Actor, ActorLogging}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
 
 
 class Listener extends Actor with ActorLogging {
   def receive = {
-//    case state: CurrentClusterState ⇒
-//      log.info("Current members: {}", state.members.mkString(", "))
-//    case MemberUp(member) ⇒
-//      log.info("Member is Up: {}", member.address)
-//    case UnreachableMember(member) ⇒
-//      log.info("Member detected as unreachable: {}", member)
-//    case MemberRemoved(member, previousStatus) ⇒
-//      log.info("Member is Removed: {} after {}",
-//        member.address, previousStatus)
-//    case _: ClusterDomainEvent ⇒ // ignore
-    case x => print(x)
+    case state: CurrentClusterState => log.info("Current members: {}", state.members.mkString(", "))
+    case MemberUp(member) => log.info("Member is Up: {}", member.address)
+    case MemberRemoved(member) =>  log.info("Member is Removed: {} after {}", member.address)
+    case _: ClusterDomainEvent ⇒ log.info("unknown event")
   }
 }
 
 object SimpleCluster extends App{
-//    System.setProperty("akka.remote.netty.tcp.port", "2551")
-//    val system = ActorSystem.create("ClusterSystem")
-//    val clusterListener = system.actorOf(Props[Listener],name = "listener")
-//    Cluster(system).subscribe(clusterListener, classOf[ClusterDomainEvent])
+    System.setProperty("akka.remote.netty.tcp.port", "2551")
+    val system = ActorSystem.create("system")
+    val listener = system.actorOf(Props[Listener],name = "listener")
+    Cluster(system).subscribe(listener, classOf[ClusterDomainEvent])
 }
