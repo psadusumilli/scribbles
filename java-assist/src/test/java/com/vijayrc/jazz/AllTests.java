fb4bc419281e1c41cb6a2459d58af3f5f5f8862b
vijayrc <vijayrc@outlook.com>
2014-03-10 22:29:35 -0400

    more work on jazz

--- java-assist/src/test/java/com/vijayrc/jazz/AllTests.java
+++ java-assist/src/test/java/com/vijayrc/jazz/AllTests.java
@@ -4,22 +4,41 @@ import javassist.*;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static com.vijayrc.jazz.Log.print;
 
 public class AllTests {
     @Test
-    public void shouldAddInheritance() throws IOException, CannotCompileException, NotFoundException, IllegalAccessException, InstantiationException {
+    public void shouldAddInheritance() throws IOException, CannotCompileException, NotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         ClassPool pool = ClassPool.getDefault();
-        CtClass ct = pool.get("com.vijayrc.jazz.Circle");
-        ct.setSuperclass(pool.get("com.vijayrc.jazz.Shape"));
-        CtMethod m = ct.getDeclaredMethod("area");
-        m.insertAfter("System.out.println(\"calculating area..\");");
-        ct.writeFile();
+        CtClass circleClass = pool.get("com.vijayrc.jazz.Circle");
+        circleClass.setSuperclass(pool.get("com.vijayrc.jazz.Shape"));
 
-        Circle c = (Circle) ct.toClass().newInstance();
+        CtMethod areaMethod = circleClass.getDeclaredMethod("area");
+        areaMethod.insertAfter("System.out.println(\"calculated area..\");");
+        circleClass.writeFile();
+
+        Circle c = (Circle) circleClass.toClass().newInstance();
         c.setRadius(10);
         print("area of circle=" + c.area());
 
+        Method getName = c.getClass().getMethod("getName");
+        print(getName.invoke(c));
     }
+    @Test
+    public void shouldCreateANewClass() throws NotFoundException, CannotCompileException {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass squareClass = pool.makeClass("com.vijayrc.Square");
+        CtClass doubleClass = pool.get("java.lang.Double");
+
+        CtConstructor constr = new CtConstructor(new CtClass[]{doubleClass},squareClass);
+        squareClass.addConstructor(constr);
+
+
+
+
+    }
+
 }
