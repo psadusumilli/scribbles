16b92911be554c346361bdc654942d49d360f361
Vijay Chakravarthy <vijayrc@outlook.com>
2014-06-04 15:44:50 -0400

    perl perl

--- perl/basics.pl
+++ perl/basics.pl
@@ -11,7 +11,7 @@ print $greet;
 my ($fname, $lname) = qw( vijay raj);
 print "$fname $lname \n";
 
-#string ops
+#string ops ---------------------------------------------------------------------------
 my $concat = "rekha"." srimalu". "\n";
 print $concat;
 
@@ -20,4 +20,25 @@ print "$multiply: length=".length($multiply). "\n";
 print substr("hey you moron",3,4)."\n";
 
 my ($w1, $w2, $w3, $w4) = split(/[0-9]{2}/,"respect12my23authorita34bitch");
-print "$w1, $w2, $w3, $w4";
+print "$w1, $w2, $w3, $w4"."\n";
+print join("-",$w1, $w2, $w3, $w4)."\n";
+
+#numbers -------------------------------------------------------------------------------
+my $z1 = 2;
+my $z2 = $z1*3;
+print "$z2\n";
+
+my $str_z1 = $z1.'';
+print $str_z1."\n";
+
+#operator------------------------------------------------------------------------------
+my $a = 0 || 1;
+my $b = 0 or 1;
+print "$a $b\n"; # a=1, since || has higher precedence than =
+
+my $c = $a==0? "hey": $b==0? "dude":"bum";
+print $c."\n"; 
+
+#file----------------------------------------------------------------------------------
+open(my $myfile,'file.txt') or print "cannot open file";
+
