de2afe788645ddcde92154164d596ba7aa7b8e7d
vijayrc <vijayrc@outlook.com>
2014-06-11 19:27:48 -0400

    moving modules to a separate folder

--- perl/modules.pl
+++ perl/modules.pl
@@ -2,6 +2,7 @@ package modules;
 use strict;
 use warnings;
 use Data::Dumper;
+use lib '/home/vijayrc/projs/VRC5/scribbles/perl/modules'; #or use PERLLIB env variable
 use dog;
 
 dog::speak;
---
+++ perl/modules/dog.pm
@@ -0,0 +1,8 @@
+package dog;
+#!/bin/perl
+use strict;
+use warnings;
+
+sub speak { print "woof!\n"; }
+1; #must return 1
+
