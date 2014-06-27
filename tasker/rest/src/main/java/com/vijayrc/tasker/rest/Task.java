79b7fa13da036a4b21c1fe2dc3d4ec552bf1a2c9
vijayrc <vijayrc@outlook.com>
2014-06-27 12:20:11 -0400

    rest url works fine

--- tasker/rest/src/main/java/com/vijayrc/tasker/rest/Task.java
+++ tasker/rest/src/main/java/com/vijayrc/tasker/rest/Task.java
@@ -1,4 +1,15 @@
 package com.vijayrc.tasker.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("tasks")
 public class Task {
+
+    @GET
+    @Produces("text/plain")
+    public String explain(){
+        return "all my tasks";
+    }
 }
