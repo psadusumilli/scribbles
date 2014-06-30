bd7c8425aad1e17471ddbc90b80dd749584426c3
vijayrc <vijayrc@outlook.com>
2014-06-30 12:39:53 -0400

    added noargs constr enhancer

--- enhancer/src/main/java/com/vijayrc/enhancer/BeanEnhancer.java
+++ enhancer/src/main/java/com/vijayrc/enhancer/BeanEnhancer.java
@@ -1,10 +1,7 @@
 package com.vijayrc.enhancer;
 
 import com.vijayrc.meta.Bean;
-import javassist.CannotCompileException;
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtMethod;
+import javassist.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.reflections.Reflections;
@@ -12,6 +9,7 @@ import org.reflections.Reflections;
 import java.lang.reflect.Field;
 
 import static org.apache.commons.lang.StringUtils.capitalize;
+import static org.apache.commons.lang.StringUtils.contains;
 
 public class BeanEnhancer implements Enhancer{
     private static Logger log = LogManager.getLogger(BeanEnhancer.class);
@@ -21,6 +19,8 @@ public class BeanEnhancer implements Enhancer{
         Reflections reflections = new Reflections(packageName);
         for (Class<?> aClass : reflections.getTypesAnnotatedWith(Bean.class)) {
             CtClass ctClass = pool.get(aClass.getName());
+
+            //~embed getter/setter
             for (Field field : aClass.getDeclaredFields()) {
                 String fieldName = field.getName();
                 CtClass fieldType = pool.get(field.getGenericType().getTypeName());
@@ -28,15 +28,26 @@ public class BeanEnhancer implements Enhancer{
                 setter(ctClass, fieldName, fieldType);
                 log.info("done getter|setter:"+fieldName);
             }
+
+            //~embed default constructor
+            boolean hasNoArgsConstructor = false;
+            for (CtConstructor constructor : ctClass.getConstructors())
+                if(constructor.getParameterTypes().length == 0) {
+                    hasNoArgsConstructor = true;
+                    log.info("no args constructor already present");
+                    break;
+                }
+            if(!hasNoArgsConstructor)
+                ctClass.addConstructor(new CtConstructor(new CtClass[]{},ctClass));
+
+            //~update class file
             ctClass.writeFile(aClass.getResource("/").getFile());
         }
     }
-
     @Override
     public String name() {
         return "BeanEnhancer";
     }
-
     private void setter(CtClass ctClass, String fieldName, CtClass fieldType) throws CannotCompileException {
         CtMethod setter  = new CtMethod(CtClass.voidType, "set"+ capitalize(fieldName), new CtClass[]{fieldType}, ctClass);
         setter.setBody("this." + fieldName + " = $1;");
--- enhancer/src/main/java/com/vijayrc/enhancer/NoArgsConstrEnhancer.java
+++ enhancer/src/main/java/com/vijayrc/enhancer/NoArgsConstrEnhancer.java
@@ -1,4 +1,36 @@
 package com.vijayrc.enhancer;
 
-public class NoArgsConstrEnhancer {
+import com.vijayrc.meta.NoArgsConstr;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.reflections.Reflections;
+
+public class NoArgsConstrEnhancer implements Enhancer {
+    private static Logger log = LogManager.getLogger(NoArgsConstrEnhancer.class);
+
+    @Override
+    public void run(String packageName) throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        Reflections reflections = new Reflections(packageName);
+
+        for (Class aClass : reflections.getTypesAnnotatedWith(NoArgsConstr.class)) {
+            CtClass ctClass = pool.get(aClass.getName());
+            boolean hasNoArgsConstructor = false;
+            for (CtConstructor constructor : ctClass.getConstructors())
+                if(constructor.getParameterTypes().length == 0) {
+                    hasNoArgsConstructor = true;
+                    log.info("no args constructor already present");
+                    break;
+                }
+            if(!hasNoArgsConstructor)
+                ctClass.addConstructor(new CtConstructor(new CtClass[]{},ctClass));
+        }
+    }
+    @Override
+    public String name() {
+        return "NoArgsConstr";
+    }
 }
