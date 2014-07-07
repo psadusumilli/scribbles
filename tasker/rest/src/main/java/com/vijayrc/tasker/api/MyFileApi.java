befc2554c70af8681f69a0a91190c309a3defdc6
vijayrc <vijayrc@outlook.com>
2014-07-07 18:05:50 -0400

    jetty runs with multipart as test compile in tasker

--- tasker/rest/src/main/java/com/vijayrc/tasker/api/MyFileApi.java
+++ tasker/rest/src/main/java/com/vijayrc/tasker/api/MyFileApi.java
@@ -3,16 +3,15 @@ package com.vijayrc.tasker.api;
 import com.vijayrc.tasker.domain.MyFile;
 import com.vijayrc.tasker.error.FileNotFound;
 import com.vijayrc.tasker.error.FileNotFoundWebError;
-import com.vijayrc.tasker.error.WebError;
 import com.vijayrc.tasker.service.MyFileService;
-import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.activation.MimetypesFileTypeMap;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.io.File;
@@ -38,16 +37,16 @@ public class MyFileApi {
         }
     }
 
-    @POST
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response create(@FormDataParam("file") File file,
-                           @FormDataParam("card") String card){
-        try {
-            Integer id = service.create(new MyFile(file,card));
-            return Response.created(uriInfo.getAbsolutePathBuilder().build(id)).build();
-        } catch (Exception e) {
-            throw new WebError(e);
-        }
-    }
+//    @POST
+//    @Consumes(MediaType.MULTIPART_FORM_DATA)
+//    public Response create(@FormDataParam("file") File file,
+//                           @FormDataParam("card") String card){
+//        try {
+//            Integer id = service.create(new MyFile(file,card));
+//            return Response.created(uriInfo.getAbsolutePathBuilder().build(id)).build();
+//        } catch (Exception e) {
+//            throw new WebError(e);
+//        }
+//    }
 
 }
