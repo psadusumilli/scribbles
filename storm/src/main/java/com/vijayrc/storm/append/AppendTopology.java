61d35f52263a7c584947a68e0cb196d54dd8b3c0
vijayrc <vijayrc@outlook.com>
2014-08-12 23:03:06 -0400

    done with storm parallelism

--- storm/src/main/java/com/vijayrc/storm/append/AppendTopology.java
+++ storm/src/main/java/com/vijayrc/storm/append/AppendTopology.java
@@ -13,12 +13,20 @@ public class AppendTopology implements MyTopology{
     @Override
     public void run() {
         TopologyBuilder builder = new TopologyBuilder();
+
+        //a spout with 2 executor threads (1 executor = 1 thread)
         builder.setSpout("s1", new WordSpout("s1"), 2);
-        builder.setBolt("b1",new AppendBolt("b1"),3).shuffleGrouping("s1");
+        //a bolt with 3 executor threads with each thread running (6/3=2) tasks serially (running bolt instance)
+        builder.setBolt("b1",new AppendBolt("b1"),3).setNumTasks(6).shuffleGrouping("s1");
+        //a bolt with only 1 executor thread
         builder.setBolt("b2",new AppendBolt("b2"),1).shuffleGrouping("b1");
+
 
         Config config = new Config();
         config.setDebug(false);
+        //2 worker process
+        config.setNumWorkers(2);
+        //combined parallelism = 2+3+1=6, 3 threads/process
 
         LocalCluster cluster = new LocalCluster();
         cluster.submitTopology(name,config,builder.createTopology());
