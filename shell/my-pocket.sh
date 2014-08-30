260ed4da3f1aa04db3ab8bb4bd6169bdb919805a
vijayaraj210 <vijayrc@outlook.com>
2014-08-30 13:17:24 -0400

    some more modifications on ux

--- shell/my-pocket.sh
+++ shell/my-pocket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function pocket(){
-	dir=.pocket
+	dir=$HOME/.pocket
 	index=$dir/.index
 	menu=$dir/.menu
 
@@ -12,27 +12,43 @@ function pocket(){
 
     case $1 in 
     	"find" ) 
+			#check if empty index
 			if [ ! -s $index ]
 			then 
-				echo "no pages yet|$ pocket add"
+				echo "no pages yet \033[36m{$ pocket add"
 				return
 			fi	
+			#search and open 
 			grep -E -i $2 $index | grep -E '(.+?)\|' -o | sed 's/|//g' > $menu 
 			grep --color=always -E '[0-9]*' $menu
-			echo "#--------------------------------------------------------#"
+			
+			echo -e "#\033[90m--------------------------------------------------------#"
 			echo -ne "\033[36mselect page\033[39m ::> "
 			read page_no
 			page=$(grep $page_no $menu | sed 's/[0-9]*:://g')
+			
 			echo "opening $page ..."
 			open -a Google\ Chrome $page
 			;;
 		"add" )
 			echo -ne "enter page|tags \033[36m{'http://vijayrc.com|code,tech,blog'}\033[39m ::>  "
 			read new_page_and_tags			 
-			new_page=$(echo $new_page_and_tags | grep -E '.*\|' -o | sed 's/\|//')						
+			new_page=$(echo $new_page_and_tags | grep -E '.*\|' -o | sed 's/\|//')			
+
+			#check if exists
+			exists=$(grep $new_page $index -c)
+			if [ exists > 0 ] 
+			then 
+				echo "page already present, want to continue? [y|n]: "
+				read ok_to_add
+				if [ ! $ok_to_add = "y"]; then return ; fi 
+			fi
+
+			#add entry
 			last_page_no=$(tail -n 1 $index | grep -E '[0-9]{1,5}::' -o | sed 's/:://' | head -1)
 			new_page_no=$[last_page_no+1]
-
+			
+			#download page content
 			echo -n "want to add page content? [y|n]: "	
 			read download
 			if [ $download = "y" ]
@@ -43,11 +59,12 @@ function pocket(){
 				echo "$new_page_no::$new_page_and_tags" >> $index
 			fi
 
+			#clean up
 			sed -i.bak 's/^ *//; s/ *$//; /^$/d' $index
 			echo "added $new_page"
 			;;
 		"list" ) 
-			echo "#--------------------------------------------------------#"
+			echo -e "\033[90m#--------------------------------------------------------#"
 			cat $index | grep -E '(.+?)\|' -o | sed 's/|//g' | grep --color=always -E '[0-9]*'
 			;;	
 		"backup" ) 
