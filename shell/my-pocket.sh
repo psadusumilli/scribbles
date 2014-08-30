8f062ca273ca84055d73287a5ec59cb23156be96
vijayaraj210 <vijayrc@outlook.com>
2014-08-30 00:51:50 -0400

    adding entry to pocket index works

--- shell/my-pocket.sh
+++ shell/my-pocket.sh
@@ -6,51 +6,54 @@ function pocket(){
 	
 	#create new files if non-existent
 	if [ ! -e $dir ]
-	then 
-		mkdir $dir
+	then mkdir $dir
 	fi	
 	if [ ! -e $index ] 
-	then 
-		touch $index
+	then touch $index
 	fi
+	rm -f $index.bak
 
 	#clean up index file
 	sed -i.bak 's/^ *//; s/ *$//; /^$/d' $index
 
 	#switch as per commands find|add|delete
     case $1 in 
+    	
     	#find and open page
     	"find" ) 
 			if [ ! -s $index ]
 			then 
-				echo "no pages yet! :: $ pocket add"
+				echo "no pages yet|$ pocket add"
 				exit 0;
 			fi	
 			grep $2 $index | grep -E '(.+?)\|' -o | sed 's/|//g' | tee $menu
 			echo "#--------------------------------------------------------#"
-			echo -n "select page :: "
+			echo -n "select page:=>"
 			read page_no
 			page=$(grep $page_no $menu | sed 's/[0-9]*:://g')
 			echo "opening $page ..."
 			open -a Google\ Chrome http://$page
 			;;
+
 		#add a new page	
 		"add" )
-			echo -n "please enter url|tags :: 'vijayrc.com|code,tech,blog' :: "
-			read newpage
+			echo -n "please enter page|tags {'vijayrc.com|code,tech,blog'}:=>  "
+			read new_page
+			last_page_no=$(tail -n 1 .pocket | grep -E '[0-9]*' -o)
+			new_page_no=$[last_page_no+1] 
+			echo "$new_page_no::$new_page" >> $index
+			sed -i.bak 's/^ *//; s/ *$//; /^$/d' $index
 
-			#pick last number and add entry to index
-			tail -n 1 .pocket | grep -E '[0-9]*' -o 
-			#read and save html
+			#fetch and save minified html
 			#tr -d '\n' < index.html > index.html.2
 			
 
-			echo "added $newpage"
+			echo "added $new_page"
 			;;
 		#usage instructions	
 		"*" ) echo "usage pocket find|add"
 	esac
-	rm $index.bak
+	rm -f $index.bak
 }
 
-pocket find news;
+pocket add;
