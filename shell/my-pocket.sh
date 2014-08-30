c3f6d83f638fb9ef413e3e4599363f873e2c3bc2
vijayaraj210 <vijayrc@outlook.com>
2014-08-29 21:52:06 -0400

    all works with terminal pocket

--- shell/my-pocket.sh
+++ shell/my-pocket.sh
@@ -6,10 +6,12 @@ function pocket(){
 		echo "new .pocket";
 	fi	
 	grep $1 .pocket | grep -E '(.+?)\|' -o | sed 's/|//g' | tee .pocket-menu
+	echo "#--------------------------------------------------------#"
 	echo -n "enter ur choice: "
 	read choice
-		grep $choice .pocket-menu
-
+	page=$(grep $choice .pocket-menu | sed 's/[0-9]*:://g')
+	echo "opening $page ..."
+	open -a Google\ Chrome http://$page
 }
 
 pocket news;
