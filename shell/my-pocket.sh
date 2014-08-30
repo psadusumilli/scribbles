a2e1b56e58aafb1fcb33902616d44c7f5e65cc50
vijayaraj210 <vijayrc@outlook.com>
2014-08-29 21:42:24 -0400

    working with menus

--- shell/my-pocket.sh
+++ shell/my-pocket.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
 function pocket(){
 	if [ ! -e .pocket ] 
 	then 
 		touch .pocket;
 		echo "new .pocket";
 	fi	
-	grep $1 .pocket | grep -E '(.+?)\|' -o | sed 's/|//g'
+	grep $1 .pocket | grep -E '(.+?)\|' -o | sed 's/|//g' | tee .pocket-menu
+	echo -n "enter ur choice: "
+	read choice
+		grep $choice .pocket-menu
+
 }
 
 pocket news;
