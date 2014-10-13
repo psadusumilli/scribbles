a145cf2675454706d78c3af32ae9b013b6567cdb
vijayrc <vijayrc@outlook.com>
2014-10-13 13:02:39 -0400

    scatter read/writes

--- nio/src/test/java/com/vijayrc/nio/AllTests.java
+++ nio/src/test/java/com/vijayrc/nio/AllTests.java
@@ -17,14 +17,46 @@ public class AllTests {
 
         int bytesToRead = channel.read(buffer);
         while(bytesToRead != -1){
-            buffer.flip();
-            StringBuilder builder = new StringBuilder();
-            while(buffer.hasRemaining()) builder.append((char) buffer.get());
-            print(builder);
+            readFrom(buffer);
             buffer.clear();
             bytesToRead = channel.read(buffer);
         }
         channel.close();
         file.close();
+    }
+
+    @Test
+    public void shouldDoAScatterReadAndWrite() throws Exception {
+        RandomAccessFile readFile = new RandomAccessFile(resource("/samples/1.txt"),"rw");
+        RandomAccessFile writeFile = new RandomAccessFile(resource("/samples/2.txt"),"rw");
+        FileChannel readChannel = readFile.getChannel();
+        FileChannel writeChannel = writeFile.getChannel();
+
+        ByteBuffer buffer1 = ByteBuffer.allocate(30);
+        ByteBuffer buffer2 = ByteBuffer.allocate(120);
+        ByteBuffer[] buffers = {buffer1, buffer2};
+
+        readChannel.read(buffers);
+
+        print("first 30 chars(bytes).....");
+        readFrom(buffer1);
+        print("next 120 chars(bytes).....");
+        readFrom(buffer2);
+
+        writeChannel.write(buffers);
+        buffer1.clear();
+        buffer2.clear();
+
+        readChannel.close();
+        writeChannel.close();
+        readFile.close();
+    }
+
+    private void readFrom(ByteBuffer buffer) {
+        buffer.flip();
+        StringBuilder builder = new StringBuilder();
+        while(buffer.hasRemaining())
+            builder.append((char) buffer.get());
+        print(builder);
     }
 }
