db97cb13a53c7411322771363e9d845ad4fc8b01
vijayaraj210 <vijayrc@outlook.com>
2015-04-28 00:45:32 -0400

    done with curl doing chunked

--- spring-boot/src/main/java/com/vijayrc/springboot/controllers/MyRestController.java
+++ spring-boot/src/main/java/com/vijayrc/springboot/controllers/MyRestController.java
@@ -4,7 +4,17 @@ import com.vijayrc.springboot.domain.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sun.net.www.http.ChunkedInputStream;
+import sun.net.www.http.ChunkedOutputStream;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static java.lang.String.format;
@@ -19,7 +29,26 @@ public class MyRestController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),  format(template, name));
+    public List<Greeting> greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        List<Greeting> greetings = new ArrayList<Greeting>();
+        for(int i=0;i<7;i++)
+            greetings.add(new Greeting(counter.incrementAndGet(),  format(template, name)));
+        return greetings;
+    }
+
+    @RequestMapping("/greeting-small")
+    public void chunked(@RequestParam(value="name", defaultValue="World") String name, HttpServletResponse response) throws IOException, InterruptedException {
+       response.setHeader("Transfer-Encoding","chunked");
+        ServletOutputStream outputStream = response.getOutputStream();
+        ChunkedOutputStream chunkedOutputStream = new ChunkedOutputStream(new PrintStream(outputStream));
+        for(int i=0;i<5;i++){
+            String s = new Greeting(counter.incrementAndGet(), format(template, name)).getContent() + new Date() + "\n";
+            chunkedOutputStream.write(s.getBytes());
+            chunkedOutputStream.flush();
+            Thread.sleep(1000);
+        }
+        chunkedOutputStream.flush();
+        chunkedOutputStream.close();
+        outputStream.close();
     }
 }
