437e87f62b667c6ccdd5f2c0831d786d4d58ceb1
vijayaraj210 <vijayrc@outlook.com>
2015-09-19 20:44:56 -0400

    startin with nathan book

---
+++ notes/bigdata.js
@@ -0,0 +1,9 @@
+Big Data
+*********
+'Incremental systems': crud complex systems
+'Lambda': wipe and start over - batch, speed, serving layers
+
+A data system answers questions based on information that was acquired in the past up to the present.
+query = func(all data)
+
+
