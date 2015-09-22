a89e4be41992405a5f93957855b2e111fae1ec15
vijayaraj210 <vijayrc@outlook.com>
2015-09-22 07:35:29 -0400

    more lamda stuff

--- notes/dep.js
+++ notes/dep.js
@@ -1,15 +1,43 @@
 
-data model - avro
-data validation
-data migration
+batch data model:
+*****************
+	fact-based data, so adding time-dimension to all incoming data.
+	graph schema, with nodes, edges and properties give the relations between facts
 
-batch
+	schema/serialization systems => enforces the data schema across lanugages; xsd, json (old mature frameworks) ; thrift, avro
+	would serialization frameworks really suffice for a rich domain like us?
+
+	cda is close to a flat graph format, but needs serdes to be written for every engagement
+	need to work on the cda alternative
+	
+	compaction must be kept in mind
+	
 
 
-pluggable tools for each layer
-resources and apis 
+batch data validation
+**********************
+	occurs at entry into batch layer ie factory, no bad data inside or do validation at 
+	same validation rules must also apply during entry into speed layer or do we wait for batch layer to correct it?
+	validation rules must be write-once and pluggable-anywhere as a library
+	does dep provide an plugpoint for normalization rules?
+
+batch view computation:
+***********************
+	pesistence forms (say custom flat-file) and processing forms (say RDD) are different
+	inspiration: GraphX RDD, Git, clojure Persistent data structures
+	no data movement, colocation of related data and processing.
+
+
 
 
 'Wild theories':
-Why not ElasticSearch for Serving layer?
+1 Why not ElasticSearch for Serving layer?
 	gives sharding, compression, replication, failover
+2 Must look into Git internals again, we might get some insights there.
+3 Must look into Persistent 
+
+
+
+pluggable tools for each layer
+resources and apis
+microservices 
