7d0c1bbce861a03e593ed6b1c785c92d02984048
vijayaraj210 <vijayrc@outlook.com>
2015-10-29 07:33:15 -0400

    more cass intro

--- cassandra/notes.js
+++ cassandra/notes.js
@@ -2,7 +2,7 @@
 0 INTRODUCTION
 ***************
     highly available, AP from CAP
-    no master, a ring of similar nodes
+    no master, a ring of similar nodes talking via 'gossip'
     linear scalability by more nodes 
     nodes that fail can easily be restored or replaced.
     multi-datacente spread
@@ -25,6 +25,56 @@
     Data written in mostly one location /  Data written in many locations
     Supports read scalability (with consistency sacrifices) / Supports read and write scalability
     Deployed in vertical scale up fashion / Deployed in horizontal scale out fashion
+
+0.2 reads/writes
+----------------
+    'write': client =>commit log (disk)=>memtable(memory)=>spill over to SStable(disk)=>compact sstables periodically
+    'read': client=>bloom filter(memory)=>partition summary(memory)=>partition index(disk)=>compression offsets(memory)=>data(disk)=>client
+
+0.3 Cluster
+------------
+    data is sharded based on default or custom partitioner
+    keyspaces are replicated across nodes: replication_factor=0,1,2...
+    sharding and replication auto-balance on adding/removing nodes
+
+0.4 data model
+--------------
+    Cassandra is a wide-row-store database that uses a highly denormalized model designed to capture and query data performantly. 
+    Although Cassandra has objects that resemble a relational database (e.g., tables, primary keys, indexes, etc.), 
+    Cassandra data modeling techniques necessarily depart from the relational tradition.
+    Can have 1000+ columns unlike rdbms
+
+    Cassandra includes:
+        'Keyspace'
+            a container for data tables and indexes; analogous to a database in many relational databases. 
+            It is also the level at which replication is defined.
+        'Table'
+            somewhat like a relational table, but capable of holding vastly large volumes of data. 
+            A table is also able to provide very fast row inserts and column level reads.
+        'Primary key'
+            used to identity a row uniquely in a table and also distribute a table’s rows across multiple nodes in a cluster.
+        'Index'
+            similar to a relational index in that it speeds some read operations; also different from relational indices in important ways.
+
+    'cql'        
+    DDL (CREATE, ALTER, DROP), DML (INSERT, UPDATE, DELETE, TRUNCATE), and query (SELECT) operations are all supported.        
+
+
+0.5 transactions
+----------------
+    Only 'AID' portion of 'ACID'. Writes to Cassandra are atomic, isolated, and durable. 
+    The 'C' of ACID—​consistency—​does not apply to Cassandra, as there is no concept of referential integrity or foreign keys.
+    tunable data consistency across a database cluster. 
+    You might want a particular request to complete if just one node responds, or you might want to wait until all nodes respond. 
+    'Tunable data consistency' is supported across single or multiple data centers, and you have a number of different consistency options from which to choose.
+    Consistency is 'configurable on a per-query basis', meaning you can decide how strong or eventual consistency should be per SELECT, INSERT, UPDATE, and DELETE operation. 
+    For example, if you need a particular transaction to be available on all nodes throughout the world, you can specify that all nodes must respond before a transaction is marked complete. 
+    On the other hand, a less critical piece of data (e.g., a social media update) may only need to be propagated eventually, so in that case, the consistency requirement can be greatly relaxed.
+    
+    Cassandra also supplies 'lightweight transactions', or a compare-and-set mechanism. 
+    Using and extending the Paxos consensus protocol (which allows a distributed system to agree on proposed data modifications with a quorum-based algorithm, 
+        and without the need for any one "master" database or two-phase commit), Cassandra offers a way to ensure a transaction isolation level similar to the serializable level offered by relational database.
+
 
 
 
