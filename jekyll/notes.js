829a580591c98bc3206c3fc563c7976d01df09ab
vijayaraj210 <vijayrc@outlook.com>
2016-01-21 06:33:37 -0500

    start9ign with jekyll

---
+++ jekyll/notes.js
@@ -0,0 +1,46 @@
+JEKYLL
+Powered by Github to generate static websites
+
+'jekyll new cep' => create new site cep
+cd cep
+'jekyll serve'  => starts an embedded server http://localhost:4000
+
+➜  cep git:(master) ✗ tree
+.
+├── about.md
+├── _config.yml
+├── css
+│   └── main.scss
+├── feed.xml
+├── _includes
+│   ├── footer.html
+│   ├── header.html
+│   ├── head.html
+│   ├── icon-github.html
+│   ├── icon-github.svg
+│   ├── icon-twitter.html
+│   └── icon-twitter.svg
+├── index.html
+├── _layouts
+│   ├── default.html
+│   ├── page.html
+│   └── post.html
+├── _posts
+│   └── 2016-01-21-welcome-to-jekyll.markdown
+├── _sass
+│   ├── _base.scss
+│   ├── _layout.scss
+│   └── _syntax-highlighting.scss
+└── _site
+    ├── about
+    │   └── index.html
+    ├── css
+    │   └── main.css
+    ├── feed.xml
+    ├── index.html
+    └── jekyll
+        └── update
+            └── 2016
+                └── 01
+                    └── 21
+                        └── welcome-to-jekyll.html
