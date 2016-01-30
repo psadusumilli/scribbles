2522516a035c0c5f1110337931dabcd94290db47
vijayaraj210 <vijayrc@outlook.com>
2016-01-30 16:24:34 -0500

    plain avro read from kafka works for spark

--- spark/spark-kafka/src/main/scala/com/vijayrc/spark/KafkaAvroReadJob.scala
+++ spark/spark-kafka/src/main/scala/com/vijayrc/spark/KafkaAvroReadJob.scala
@@ -4,15 +4,15 @@ import java.io.ByteArrayOutputStream
 import java.util
 
 import kafka.serializer.DefaultDecoder
-import org.apache.avro.io.EncoderFactory
-import org.apache.avro.specific.SpecificDatumWriter
+import org.apache.avro.io.{DecoderFactory, EncoderFactory}
+import org.apache.avro.specific.{SpecificDatumReader, SpecificDatumWriter}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import org.apache.log4j.Logger
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
 
 import scala.util.Random
 
@@ -27,7 +27,9 @@ object KafkaAvroReadJob {
    * run all samples
    */
   def main(args: Array[String]): Unit = {
-    if (args.nonEmpty && args {0}.equalsIgnoreCase("write")) writeAvro()
+    if (args.nonEmpty && args {
+      0
+    }.equalsIgnoreCase("write")) writeAvro()
     else readAvro()
   }
 
@@ -42,17 +44,18 @@ object KafkaAvroReadJob {
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
     val producer = new KafkaProducer[String, Array[Byte]](props)
 
-    for (i <- 0 to 10) {
+    for (i <- 0 to 50) {
       val clickBytes = serializeClickEvent(newRandomClickEvent)
       val message = new ProducerRecord[String, Array[Byte]]("test", null, clickBytes)
       producer.send(message)
-      logger.info("sent message" + i)
+      println("sent message" + i)
       Thread sleep 1000
     }
 
     def newRandomClickEvent: ClickEvent = {
       val userId = Random.nextInt(5)
       val productId = Random.nextInt(5)
+      println("click: " + userId+"->"+productId)
       new ClickEvent(userId, productId)
     }
 
@@ -76,54 +79,56 @@ object KafkaAvroReadJob {
    * read an avro event to kafka
    */
   def readAvro(): Unit = {
-
-    val sparkConf = new SparkConf().setAppName("WindowClickCount").setMaster("local[2]")
+    val sparkConf = new SparkConf().setAppName("click-count").setMaster("local[2]")
 
     val ssc = new StreamingContext(sparkConf, Seconds(2))
-
-    // Because we're using .reduceByKeyAndWindow, we need to persist it to disk
     ssc.checkpoint("./checkpointDir")
 
-    val kafkaConf = Map(
-      "metadata.broker.list" -> "localhost:9092", // Default kafka broker list location
-      "zookeeper.connect" -> "localhost:2181", // Default zookeeper location
+    val kafkaConf = Map("metadata.broker.list" -> "localhost:9092",
+      "zookeeper.connect" -> "localhost:2181",
       "group.id" -> "kafka-spark-streaming-example",
       "zookeeper.connection.timeout.ms" -> "1000")
 
-    val topicMap = Map("test" ->1)
+    val topicMap = Map("test" -> 1)
+    val userNameMapRDD = ssc.sparkContext.parallelize(Array((1, "Joe"), (2, "Michelle"), (3, "David"), (4, "Anthony"), (5, "Lisa")))
+    val productNameMapRDD = ssc.sparkContext.parallelize(Array((1, "Legos"), (2, "Books"), (3, "Board Games"), (4, "Food"), (5, "Computers")))
 
-    // Create a new stream which can decode byte arrays.  For this exercise, the incoming stream only contain user and product Ids
+    // read from topic
     val lines = KafkaUtils.createStream[String, Array[Byte], DefaultDecoder, DefaultDecoder](ssc, kafkaConf, topicMap, StorageLevel.MEMORY_ONLY_SER).map(_._2)
 
-    // Create a RDD containing the mapping of ids to names.  In practice, these mappings will come from HDFS/S3/(a real data source)
-    val userNameMapRDD = ssc.sparkContext.parallelize(Array((1,"Joe"), (2, "Michelle"), (3, "David"), (4, "Anthony"), (5, "Lisa")))
-    val productNameMapRDD = ssc.sparkContext.parallelize(Array((1,"Legos"), (2, "Books"), (3, "Board Games"), (4, "Food"), (5, "Computers")))
-
-    // Join the stream with the userNameMapRDD to map the userName to the userId
-    val mappedUserName = lines.transform{rdd =>
+    //  deserialize clickevent => userid, prodid => username, prodid
+    val mappedUserName = lines.transform {
+      rdd =>
       val clickRDD: RDD[(Int, Int)] = rdd.map { bytes => AvroUtil.clickEventDecode(bytes) }.map { clickEvent =>
         (clickEvent.getUserId: Int) -> clickEvent.getProductId
       }
-
-      clickRDD.join(userNameMapRDD).map { case (userId, (productId, userName)) => (userName, productId)}
+      clickRDD.join(userNameMapRDD).map { case (userId, (productId, userName)) => (userName, productId) } // Joe->1,Anthony->3
     }
 
-    // Join the stream with the productNameMapRDD to map the productName to the productId
-    val mappedProductId = mappedUserName.transform{ rdd =>
+    // username, prodid => username, prodname
+    val mappedProductId = mappedUserName.transform { rdd =>
       val productRDD = rdd.map { case (userName, productId) => (productId: Int, userName) }
-
-      productRDD.join(productNameMapRDD).map { case (productId, (productName, userName)) => (userName, productName)}
+      productRDD.join(productNameMapRDD).map { case (productId, (productName, userName)) => (userName, productName) } //Joe -> Legos, Anthony -> BoardGames
     }
 
-    // Get a count of all the users and the products they visited in the last 10 minutes, refreshing every 2 seconds
+    // get a count of all the users and the products they visited in the last 10 minutes, refreshing every 2 seconds
     val clickCounts = mappedProductId.map(x => (x, 1L))
-      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2).map { case ((productName, userName), count) =>
-      (userName, productName, count)
-    }
-
-    clickCounts.print // Print out the results.  Or we can produce new kafka events containing the mapped ids.
+      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
+      .map { case ((productName, userName), count) =>   (userName, productName, count)  }
+    clickCounts.print()
 
     ssc.start()
     ssc.awaitTermination()
+  }
+
+
+}
+
+object AvroUtil {
+  val reader = new SpecificDatumReader[ClickEvent](ClickEvent.getClassSchema)
+
+  def clickEventDecode(bytes: Array[Byte]): ClickEvent = {
+    val decoder = DecoderFactory.get.binaryDecoder(bytes, null)
+    reader.read(null, decoder)
   }
 }
