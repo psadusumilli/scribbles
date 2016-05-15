735a27ded17d7b8aab8f9e1a473c5dab11a9d349
vijayaraj210 <vijayrc@outlook.com>
2016-05-14 21:06:11 -0400

    more cass notes

--- cassandra/notes.js
+++ cassandra/notes.js
@@ -3,18 +3,15 @@
 ***************
 “Apache Cassandra is an open source, distributed, decentralized, elastically scalable,
 highly available, fault-tolerant, tuneably consistent, column-oriented database that
-bases its distribution design on Amazon’s Dynamo and its data model on Google’s
-Bigtable. Created at Facebook, it is now used at some of the most popular sites on the
-Web.”
-consistency level is a setting that clients must specify on every operation and that allows you to decide how many replicas in the cluster must acknowledge a write op- eration or respond to a read operation in order to be considered successful.
+bases its distribution design on Amazon’s Dynamo and its data model on Google Bigtable.
+Created at Facebook, it is now used at some of the most popular sites on the Web.”
 
 0.0 general rdbms
 ----------------------
-    ACID,
-    2 phase commit  to scale across machines
+    ACID, 2 phase commit  to scale across machines
     sharding - feature-based, key-based, lookup table
     ORM & schema not matching real domain entities
-    denormalizing
+    denormalizing & star schemas
 
 0.1 rdbms comparision
 ----------------------
@@ -29,57 +26,7 @@ consistency level is a setting that clients must specify on every operation and 
     Supports read scalability (with consistency sacrifices) / Supports read and write scalability
     Deployed in vertical scale up fashion / Deployed in horizontal scale out fashion
 
-0.2 reads/writes
-----------------
-    'write': client =>commit log (disk)=>memtable(memory)=>spill over to SStable(disk)=>compact sstables periodically
-    'read': client=>bloom filter(memory)=>partition summary(memory)=>partition index(disk)=>compression offsets(memory)=>data(disk)=>client
-
-0.3 Cluster
-------------
-    data is sharded based on default or custom partitioner
-    keyspaces are replicated across nodes: replication_factor=0,1,2...
-    sharding and replication auto-balance on adding/removing nodes
-
-0.4 data model
---------------
-    Cassandra is a wide-row-store database that uses a highly denormalized model designed to capture and query data performantly.
-    Although Cassandra has objects that resemble a relational database (e.g., tables, primary keys, indexes, etc.),
-    Cassandra data modeling techniques necessarily depart from the relational tradition.
-    Can have 1000+ columns unlike rdbms
-
-    Cassandra includes:
-        'Keyspace'
-            a container for data tables and indexes; analogous to a database in many relational databases.
-            It is also the level at which replication is defined.
-        'Table'
-            somewhat like a relational table, but capable of holding vastly large volumes of data.
-            A table is also able to provide very fast row inserts and column level reads.
-        'Primary key'
-            used to identity a row uniquely in a table and also distribute a table’s rows across multiple nodes in a cluster.
-        'Index'
-            similar to a relational index in that it speeds some read operations; also different from relational indices in important ways.
-
-    'cql'
-    DDL (CREATE, ALTER, DROP), DML (INSERT, UPDATE, DELETE, TRUNCATE), and query (SELECT) operations are all supported.
-
-
-0.5 transactions
-----------------
-    Only 'AID' portion of 'ACID'. Writes to Cassandra are atomic, isolated, and durable.
-    The 'C' of ACID—​consistency—​does not apply to Cassandra, as there is no concept of referential integrity or foreign keys.
-    tunable data consistency across a database cluster.
-    You might want a particular request to complete if just one node responds, or you might want to wait until all nodes respond.
-    'Tunable data consistency' is supported across single or multiple data centers, and you have a number of different consistency options from which to choose.
-    Consistency is 'configurable on a per-query basis', meaning you can decide how strong or eventual consistency should be per SELECT, INSERT, UPDATE, and DELETE operation.
-    For example, if you need a particular transaction to be available on all nodes throughout the world, you can specify that all nodes must respond before a transaction is marked complete.
-    On the other hand, a less critical piece of data (e.g., a social media update) may only need to be propagated eventually, so in that case, the consistency requirement can be greatly relaxed.
-
-    Cassandra also supplies 'lightweight transactions', or a compare-and-set mechanism.
-    Using and extending the Paxos consensus protocol (which allows a distributed system to agree on proposed data modifications with a quorum-based algorithm,
-        and without the need for any one "master" database or two-phase commit), Cassandra offers a way to ensure a transaction isolation level similar to the serializable level offered by relational database.
-
-
--------------------------------------------------------------------------------------------------------------------------------------------
+//.........................................................................................................................................................
 1 QUICKSTART
 *************
 
@@ -135,40 +82,96 @@ consistency level is a setting that clients must specify on every operation and 
     (2 rows)
     cqlsh:mykeyspace>
 
-------------------------------------------------------------------------------------------------------------------------------------------------------
+//.........................................................................................................................................................
+
 2 ARCHITECTURE
 ***************
 2 FEATURES
- 2.1 distributed, no-master, gossip protocol
- 2.2 elastic scalability, up and down by adding nodes, balances automatically
- 2.3 highly available and fault tolerant
- 2.4 tuneable consistency
+  'distributed & elastic scalability',
+    no-master, gossip protocol
+    scales well with multi-core and multiple datacenters.
+    high write throughput.
+    good for multi node deployments of significant scale.
+    data is sharded based on default or custom partitioner
+    keyspaces are replicated across nodes: replication_factor=0,1,2...
+    sharding and replication auto-balance on adding/removing nodes
+    scale up and down by adding nodes, balances automatically
+    highly available and fault tolerant
+//.........................................................................................
+  'Tunable data consistency'
+    is supported across single or multiple data centers, and you have a number of different consistency options from which to choose.
+    Consistency is 'configurable on a per-query basis', meaning you can decide how strong or eventual consistency should be per SELECT, INSERT, UPDATE, and DELETE operation.
+    For example, if you need a particular transaction to be available on all nodes throughout the world, you can specify that all nodes must respond before a transaction is marked complete. On the other hand, a less critical piece of data (e.g., a social media update) may only need to be propagated eventually, so in that case, the consistency requirement can be greatly relaxed.
     in general consistency types => strict, causal (vector clocks), eventual
-    2.4.1 replication factor -> number of nodes to propagate a write action
-    2.4.2 consistency factor -> number of nodes to acknowledge a write action
-      consistency factor < replication factor => available system
-      consistency factor = replication factor => strict consistency
-      consistency factor > replication factor => not possible.
- 2.5 CAP
+
+      'replication factor' -> number of nodes to propagate a write action
+      'consistency factor' -> number of nodes to acknowledge a write action
+            consistency factor < replication factor => available system
+            consistency factor = replication factor => strict consistency
+            consistency factor > replication factor => not possible.
+//.........................................................................................
+ 'CAP'
       CA -> many nodes with replicated data, must block for every write to synch all nodes to be consistent.
       CP -> many nodes with partitioned data, can lose a shard when the node goes down.
       AP -> many nodes with partitioned & replicated data, consistency is eventual.
- 2.6 data store:  internally, cassandra is more row-oriented than column-oriented, stores as multi-dimensional hashmap.
- 2.7 schema free
- 2.8 scales well with multi-core and multiple datacenters.
- 2.9 high write throughput.
- 2.10 good for multi node deployments of significant scale.
+      Only 'AID' portion of 'ACID'. Writes to Cassandra are atomic, isolated, and durable.
+      The 'C' of ACID—​consistency—​does not apply to Cassandra, as there is no concept of referential integrity or foreign keys.
+      tunable data consistency across a database cluster.
+      You might want a particular request to complete if just one node responds, or you might want to wait until all nodes respond.
 
-Nodes persist WRITE to a 'commit log'.
-Data is then indexed and written to an in-memory structure, called a 'memtable', which resembles a write-back cache.
-Once the memory structure is full, the data is written to disk in an 'SSTable' data file.
-A sorted string table (SSTable) is an immutable data file to which Cassandra writes memtables periodically.
-SSTables are append only and stored on disk sequentially and maintained for each Cassandra table.
-All writes are automatically 'partitioned and replicated' throughout the cluster using 'gossip' every second.
-Using 'compaction' Cassandra periodically consolidates SSTables, discarding obsolete data and tombstones (an indicator that data was deleted).
+      Cassandra also supplies 'lightweight transactions', or a compare-and-set mechanism.
+      Using and extending the Paxos consensus protocol (which allows a distributed system to agree on proposed data modifications with a quorum-based algorithm,
+          and without the need for any one "master" database or two-phase commit), Cassandra offers a way to ensure a transaction isolation level similar to the serializable level offered by relational database.
 
-commit log=>memtable=>sstable
-node=>datacenter(group of nodes in a location)=>cluster(across datacenters)
+//.........................................................................................
+ 'data store'
+      Cassandra is a wide-row-store database that uses a highly denormalized model designed to capture and query data performantly.
+      Although Cassandra has objects that resemble a relational database (e.g., tables, primary keys, indexes, etc.),
+      Cassandra data modeling techniques necessarily depart from the relational tradition.
+      Can have 1000+ columns unlike rdbms
+      internally, cassandra is more row-oriented than column-oriented, stores as multi-dimensional hashmap.
+      “Sparse” means that for any given row you can have one or more columns,
+      but each row doesn’t need to have all the same columns as other rows like it (as in a relational model)
+      each row has a unique key
+
+      Cassandra includes:
+          'Keyspace'
+              a container for data tables and indexes; analogous to a database in many relational databases.
+              It is also the level at which replication is defined.
+          'Table'
+              somewhat like a relational table, but capable of holding vastly large volumes of data.
+              A table is also able to provide very fast row inserts and column level reads.
+          'Primary key'
+              used to identity a row uniquely in a table and also distribute a table’s rows across multiple nodes in a cluster.
+          'Index'
+              similar to a relational index in that it speeds some read operations; also different from relational indices in important ways.
+
+      'cql'
+      DDL (CREATE, ALTER, DROP), DML (INSERT, UPDATE, DELETE, TRUNCATE), and query (SELECT) operations are all supported.
+
+ //.........................................................................................
+ 'schema free'
+
+
+
+//.......................................................................................................................................................
+INTERNALS
+
+2.0
+ 'How READ/WRITE happens'
+    'write': client =>commit log (disk)=>memtable(memory)=>spill over to SStable(disk)=>compact sstables periodically
+    'read': client=>bloom filter(memory)=>partition summary(memory)=>partition index(disk)=>compression offsets(memory)=>data(disk)=>client
+
+    Nodes persist WRITE to a 'commit log'.
+    Data is then indexed and written to an in-memory structure, called a 'memtable', which resembles a write-back cache.
+    Once the memory structure is full, the data is written to disk in an 'SSTable' data file.
+    A sorted string table (SSTable) is an immutable data file to which Cassandra writes memtables periodically.
+    SSTables are append only and stored on disk sequentially and maintained for each Cassandra table.
+    All writes are automatically 'partitioned and replicated' throughout the cluster using 'gossip' every second.
+    Using 'compaction' Cassandra periodically consolidates SSTables, discarding obsolete data and tombstones (an indicator that data was deleted).
+
+    commit log=>memtable=>sstable
+    node=>datacenter(group of nodes in a location)=>cluster(across datacenters)
 
 2.1
 'gossip' is a peer-to-peer protocol that transmits state data every sec to synch up.
@@ -188,7 +191,6 @@ This leads to non-contiguous data distribution of partitioned data.
     64-bit hash value, possible range of hash values is from 2^-63 to 2^+63-1.
 • 'RandomPartitioner': uniformly distributes data across the cluster based on MD5 hash values. 0-(2^127-1)
 • 'ByteOrderedPartitioner': keeps an ordered distribution of data lexically by key bytes
-
 
 2.4 replication
 'replication_factor'=1 means only 1 copy in 1 node, 2 means 2 copies in 2 nodes. there is no primary version, all are copies since no master.
